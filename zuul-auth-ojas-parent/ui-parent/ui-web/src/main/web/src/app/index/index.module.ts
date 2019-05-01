3b72844d0aec00a36e687cd25304a1d0c97b8ac5
Spandana Kanchanapally <kspandana@corp.ojas-it.com>
2019-05-01 16:12:49 +0530

    adding login password show and hide commented

--- zuul-auth-ojas-parent/ui-parent/ui-web/src/main/web/src/app/index/index.module.ts
+++ zuul-auth-ojas-parent/ui-parent/ui-web/src/main/web/src/app/index/index.module.ts
@@ -4,14 +4,12 @@ import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { ForgotComponent } from './forgot/forgot.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { ShowHidePasswordModule } from 'ngx-show-hide-password';
 @NgModule({
   declarations: [LoginComponent, SignUpComponent, ForgotComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    FormsModule,
-    ShowHidePasswordModule
+    FormsModule
   ]
 })
 export class IndexModule { }

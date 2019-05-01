df01f3510340f6506dda22c241b02e0be640c517
Spandana Kanchanapally <kspandana@corp.ojas-it.com>
2019-05-01 17:13:47 +0530

    modified sql queries

--- zuul-auth-ojas-parent/ojas-obs-employment-details/src/main/resources/obs-queries.sql
+++ zuul-auth-ojas-parent/ojas-obs-employment-details/src/main/resources/obs-queries.sql
@@ -6,8 +6,8 @@ INSERT_EMPLOYMENT_DETAILS_STMT = INSERT into obs_employmentdetails(employee_id,j
 
  UPDATE_EMPLOYMENT_DETAILS_STMT = UPDATE obs_employmentdetails set resource_type=?,bond_status=?,resignation_date=?,exit_date=?,separation_type=?,flag=?,updated_by=?,updated_date=now() where id=?; 
 
-DELETE_EMPLOYMENT_DETAILS_STMT = UPDATE obs_employmentdetails set flag=? where id=?
+DELETE_EMPLOYMENT_DETAILS_STMT = UPDATE obs_employmentdetails set flag=? where id=?;
 
-GET_EMPLOYMENT_DETAILS_BY_EMPLOYEE_ID_STMT= select * from obs_employmentdetails where employee_id=? and flag=1
+GET_EMPLOYMENT_DETAILS_BY_EMPLOYEE_ID_STMT= select * from obs_employmentdetails where employee_id=? and flag=1;
 
-GET_EMPLOYMENT_DETAILS_BY_ID_STMT=select * from obs_employmentdetails where id=? and flag=1
+GET_EMPLOYMENT_DETAILS_BY_ID_STMT=select * from obs_employmentdetails where id=? and flag=1;

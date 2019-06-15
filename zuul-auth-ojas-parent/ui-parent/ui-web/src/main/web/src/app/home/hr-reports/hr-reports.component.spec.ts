60a188c474634ca3da502ce9f4441d9924f2f417
tshiva <tshiva@host.docker.internal>
2019-06-15 16:49:33 +0530

    added component for hr reports

--- zuul-auth-ojas-parent/ui-parent/ui-web/src/main/web/src/app/home/hr-reports/hr-reports.component.spec.ts
+++ zuul-auth-ojas-parent/ui-parent/ui-web/src/main/web/src/app/home/hr-reports/hr-reports.component.spec.ts
@@ -1,1 +1,25 @@
-added hr-reports.component.spec.ts
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HrReportsComponent } from './hr-reports.component';
+
+describe('HrReportsComponent', () => {
+  let component: HrReportsComponent;
+  let fixture: ComponentFixture<HrReportsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HrReportsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HrReportsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

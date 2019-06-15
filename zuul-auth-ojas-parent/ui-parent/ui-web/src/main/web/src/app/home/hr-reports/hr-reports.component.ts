298a34343579be40040b313e1ca60306dea6f9a2
tshiva <tshiva@host.docker.internal>
2019-06-15 16:50:19 +0530

    added component for hr reports

--- zuul-auth-ojas-parent/ui-parent/ui-web/src/main/web/src/app/home/hr-reports/hr-reports.component.ts
+++ zuul-auth-ojas-parent/ui-parent/ui-web/src/main/web/src/app/home/hr-reports/hr-reports.component.ts
@@ -1,1 +1,15 @@
-added hr-reports.component.ts
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hr-reports',
+  templateUrl: './hr-reports.component.html',
+  styleUrls: ['./hr-reports.component.scss']
+})
+export class HrReportsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

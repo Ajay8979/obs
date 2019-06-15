e6ae1f1835045c75bab20244076696e52a82bc0b
tshiva <tshiva@host.docker.internal>
2019-06-15 12:58:31 +0530

    added spec.ts

--- zuul-auth-ojas-parent/ui-parent/ui-web/src/main/web/src/app/home/career/career.component.spec.ts
+++ zuul-auth-ojas-parent/ui-parent/ui-web/src/main/web/src/app/home/career/career.component.spec.ts
@@ -1,1 +1,25 @@
-added career.component.spec.ts
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CareerComponent } from './career.component';
+
+describe('CareerComponent', () => {
+  let component: CareerComponent;
+  let fixture: ComponentFixture<CareerComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CareerComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CareerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

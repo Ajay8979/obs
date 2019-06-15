71089994f7bfeefb32cb6436db0d5ebe2648b2bd
tshiva <tshiva@host.docker.internal>
2019-06-15 16:56:36 +0530

    added for new component hr role

--- zuul-auth-ojas-parent/ui-parent/ui-web/src/main/web/src/app/home/hr-role/hr-role.component.spec.ts
+++ zuul-auth-ojas-parent/ui-parent/ui-web/src/main/web/src/app/home/hr-role/hr-role.component.spec.ts
@@ -1,1 +1,25 @@
-added hr-role.component.spec.ts
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HrRoleComponent } from './hr-role.component';
+
+describe('HrRoleComponent', () => {
+  let component: HrRoleComponent;
+  let fixture: ComponentFixture<HrRoleComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HrRoleComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HrRoleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

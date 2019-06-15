efe60b77b90c75f1a899050f3985d0246bf1d44c
tshiva <tshiva@host.docker.internal>
2019-06-15 17:27:36 +0530

    modified message to statusMessage

--- zuul-auth-ojas-parent/ui-parent/ui-web/src/main/web/src/app/home/employee/employeeedit/experience/experience.component.ts
+++ zuul-auth-ojas-parent/ui-parent/ui-web/src/main/web/src/app/home/employee/employeeedit/experience/experience.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import swal from 'sweetalert';
 import { HrmsService } from 'src/app/home/services/hrms.service';
+import { DataService } from 'src/app/home/services/data.service';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+
 
 
 @Component({
@@ -9,13 +13,56 @@ import { HrmsService } from 'src/app/home/services/hrms.service';
   styleUrls: ['./experience.component.scss']
 })
 export class ExperienceComponent implements OnInit {
+  
+  subscription: Subscription;
+  message: any;
+ public eid:any;
+  loggeduser: string;
+  emp: any;
+  empbasic: any;
+  empbasicinfo: any;
+  constructor(private hrms:HrmsService, private dataservice:DataService, private paramroute:ActivatedRoute) {
+    this.eid=this.dataservice.paramId;
+    this.loggeduser=localStorage.getItem('UserName');
 
-  constructor(private hrms:HrmsService) { }
+   }
+  // getempId(){
+    
+  // }
 
   ngOnInit() {
-    this.getEmpExp();
-  }
+    
+    this.getEmpExp(); 
 
+    this.getempdata();
+
+  
+    
+  
+  
+
+  }
+ 
+
+
+getempdata(){
+
+   var empinfo = 
+  {
+
+    "employeeInfo" :[{
+          "employeeId" : this.eid
+
+    }],
+    "transactionType" : "getbyid"
+    
+}
+   this.hrms.getempinfo(empinfo).subscribe(res =>{
+  this.empbasic =res;
+  this.empbasicinfo= this.empbasic.employeeInfo;
+   console.log(this.empbasicinfo);
+   })
+  }
 // ----Employee Experience Details Starts---------------------------------------------
 empexp : any;
 empexpdetails :any;
@@ -24,15 +71,28 @@ saveRes :any;
 //---getting employee details---------------------------------------------------
 getEmpExp()
 {
-  var requestObj = 
+  var requestObj =
+//   {
+//     "employeeExperienceDetails" :[ {
+         
+    
+
+            
+//     }],
+//              "transactionType":"getall",
+//             "sessionId" : "any String" 
+// } 
+
   {
     "employeeExperienceDetails" :[ {
+            "employee_Id": this.eid
 
             
     }],
-             "transactionType":"getall",
+             "transactionType":"getById",
             "sessionId" : "any String" 
-  }
+}
+  
 
   this.hrms.getEmployeeExperienceDetails(requestObj).subscribe(response =>{
     this.empexp = response;
@@ -73,6 +133,7 @@ EmpExpArr:any;
         
         [ 
           {
+                 "id":this.editemparrlist.id,
                  "company_name":this.editemparrlist.company_name,
                  "joining_date":this.editemparrlist.joining_date,
                  "exit_date":this.editemparrlist.exit_date,
@@ -80,40 +141,35 @@ EmpExpArr:any;
                  "location":this.editemparrlist.location,
                  "is_current_company":this.editemparrlist.is_current_company,
                  //"is_current_company":1,
-                 "employee_Id":this.editemparrlist.employee_Id,
+                 "employee_Id":this.eid,
                  "first_Reference_Name":this.editemparrlist.first_Reference_Name,
                  "first_Reference_Contact":this.editemparrlist.first_Reference_Contact,
-                 "second_Reference_Name":"phani",
-                 "second_Reference_Contact":1234567890,
-                 "created_by":19196,
-                 "created_date":"2019-04-22T06:29:14.000+0000",
-                  "updated_by": 123,
-                  "updated_date": "2019-04-22T06:29:14.000+0000"
-                 
-              
+                 "second_Reference_Name":this.editemparrlist.first_Reference_Name,
+                  "second_Reference_Contact":this.editemparrlist.first_Reference_Contact,
+                 "created_by":this.loggeduser
           }
         ],
         
-            "transactionType" : "save",
-             "sessionId" : "any String" 
+            "transactionType" : "save"
     }
 
     this.hrms.saveEmployeeExperienceDetails(savReqObj).subscribe(res =>{
       this.saveRes = res;
       console.log(this.saveRes);
       
-        if(this.saveRes.statusMessage == "EmployeeExperienceDetails saved successfully")
+        if(this.saveRes.message == "EmployeeExperienceDetails saved successfully")
         {
-         swal(this.saveRes.statusMessage,"","success");
-         //this.getEmpExp();
+         swal(this.saveRes.message,"","success");
+         this.getEmpExp();
        }
-     this.getEmpExp();
+     //this.getEmpExp();
     })
 
   }
 
 //---deleting employee experience---------------------------
 deleteres:any;
+details;
 deleteEmpExpArr:any;
 
 
@@ -135,9 +191,9 @@ this.deleteres = res;
 this.deleteEmpExpArr = this.deleteres.employeeExperienceDetails;
 console.log(this.deleteres);
 
- if(this.deleteres.statusMessage == "EmployeeExperienceDetails deleted sucesfully")
+ if(this.deleteres.message == "EmployeeExperienceDetails deleted sucesfully")
  {
-  swal(this.deleteres.statusMessage,"","success");
+  swal(this.deleteres.message,"","success");
   this.getEmpExp();
  }
 //this.getEmpExp();
@@ -183,6 +239,24 @@ this.isCreated = true;
 
 editempexpbyid(emp)
 {
+console.log(emp);
+
+
+// this.editemparrlist.employee_Id=emp.employee_Id;
+// this.editemparrlist.company_name=emp.company_name;
+// this.editemparrlist.salary=emp.salary;
+// this.editemparrlist.location=emp.location;
+// this.editemparrlist.joining_date=emp.joining_date;
+// this.editemparrlist.exit_date=emp.exit_date;
+// this.editemparrlist.is_current_company=emp.is_current_company;
+
+// this.editemparrlist.first_Reference_Name=emp.first_Reference_Name;
+// this.editemparrlist.second_Reference_Name=emp.second_Reference_Name;
+// this.editemparrlist.second_Reference_Contact=emp.second_Reference_Contact;
+// this.editemparrlist.updated_by=emp.updated_by;
+// this.editemparrlist.created_by=emp.created_by;
+// this.editemparrlist.created_date=emp.created_date;
+// this.editemparrlist.updated_date=emp.updated_date;
 
 this.isUpdate = true;
 this.isCreated = false;
@@ -236,11 +310,9 @@ console.log("this.editemparrlist",this.editemparrlist);
           "employee_Id":this.editemparrlist.employee_Id,
           "first_Reference_Name":this.editemparrlist.first_Reference_Name,
           "first_Reference_Contact":this.editemparrlist.first_Reference_Contact,
-          "second_Reference_Name":"phani",
-          "second_Reference_Contact":"gpv",
-          "updated_by":this.editemparrlist.updated_by,
-        
-          
+          "second_Reference_Name":this.editemparrlist.first_Reference_Name,
+          "second_Reference_Contact":this.editemparrlist.first_Reference_Contact,
+          "updated_by":this.loggeduser
   }],
             "transactionType":"update",
            "sessionId" : "any String" 
@@ -252,9 +324,9 @@ this.hrms.updateEmployeeExperienceDetails(updateReqObj).subscribe(res => {
   console.log(this.updateres);
   // this.updateEmpExpArr = this.updateres.employeeExperienceDetails;
   // console.log(this.updateEmpExpArr);
-   if(this.updateres.statusMessage == "EmployeeExperienceDetails updated successfully")
+   if(this.updateres.message == "EmployeeExperienceDetails updated successfully")
        {
-        swal(this.updateres.statusMessage,"","success");
+        swal(this.updateres.message,"","success");
         this.getEmpExp();
       }
      //this.getEmpExp();

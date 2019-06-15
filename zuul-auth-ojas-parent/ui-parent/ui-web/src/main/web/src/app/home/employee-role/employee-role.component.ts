107f173d000706585fee5158aa8921172d61ed45
tshiva <tshiva@host.docker.internal>
2019-06-15 13:05:57 +0530

    added ts file

--- zuul-auth-ojas-parent/ui-parent/ui-web/src/main/web/src/app/home/employee-role/employee-role.component.ts
+++ zuul-auth-ojas-parent/ui-parent/ui-web/src/main/web/src/app/home/employee-role/employee-role.component.ts
@@ -1,1 +1,15 @@
-added employee-role.component.ts
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-role',
+  templateUrl: './employee-role.component.html',
+  styleUrls: ['./employee-role.component.scss']
+})
+export class EmployeeRoleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

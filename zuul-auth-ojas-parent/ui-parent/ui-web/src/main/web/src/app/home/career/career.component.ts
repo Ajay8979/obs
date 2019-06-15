a738b464a386b0009598bb95f69c38d6fcb5bf4c
tshiva <tshiva@host.docker.internal>
2019-06-15 12:59:02 +0530

    added ts file

--- zuul-auth-ojas-parent/ui-parent/ui-web/src/main/web/src/app/home/career/career.component.ts
+++ zuul-auth-ojas-parent/ui-parent/ui-web/src/main/web/src/app/home/career/career.component.ts
@@ -1,1 +1,15 @@
-added career.component.ts
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-career',
+  templateUrl: './career.component.html',
+  styleUrls: ['./career.component.scss']
+})
+export class CareerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

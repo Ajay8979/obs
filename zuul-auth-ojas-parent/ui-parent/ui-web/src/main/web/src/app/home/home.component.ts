30035ce7f67c26ffe9e22e8813d97c7c4bf6f6e4
tshiva <tshiva@host.docker.internal>
2019-06-15 11:34:31 +0530

    added multiple roles

--- zuul-auth-ojas-parent/ui-parent/ui-web/src/main/web/src/app/home/home.component.ts
+++ zuul-auth-ojas-parent/ui-parent/ui-web/src/main/web/src/app/home/home.component.ts
@@ -8,7 +8,12 @@ declare var $: any;
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+username:any;
+  role: any=[];
+  role0:any=[];
+  role1: any=[];
+  role2: any=[];
+  changerole: string;
   constructor(private authService: AuthService, private routerNavigate: Router) { }
 
   logoutAction() {
@@ -16,9 +21,37 @@ export class HomeComponent implements OnInit {
       this.routerNavigate.navigate(['login'])
     }
   }
+  changeRole(){
+    if(this.role=='ROLE_ADMIN')
+    {
+    this.changerole = localStorage.getItem('Role');
 
+    console.log(this.changerole)
+    this.role=this.changerole[0][0]
+  }
+
+   else if(this.role=='ROLE_HR')
+   {
+    this.changerole = localStorage.getItem('Role1');
+
+    console.log(this.changerole)
+    this.role=this.changerole[0][1]
+   }
+   else if(this.role=='ROLE_USER')
+   {
+    this.changerole = localStorage.getItem('Role2');
+
+    console.log(this.changerole)
+    this.role=this.changerole[0][2]
+   }
+  }
 
   ngOnInit() {
+this.username=localStorage.getItem('UserName');
+this.role=localStorage.getItem('Role');
+//  this.role0=localStorage.getItem('Role0');
+// this.role1=localStorage.getItem('Role1');
+//  this.role2=localStorage.getItem('Role2');
 
     $(document).ready(function () {
       $(".push_menu").click(function () {

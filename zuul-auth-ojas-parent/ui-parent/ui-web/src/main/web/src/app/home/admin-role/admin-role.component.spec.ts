bc7eeafed9bc7c2fecfa7c39531d67ea80ca97c4
tshiva <tshiva@host.docker.internal>
2019-06-15 12:52:02 +0530

    added component.spec.ts

--- zuul-auth-ojas-parent/ui-parent/ui-web/src/main/web/src/app/home/admin-role/admin-role.component.spec.ts
+++ zuul-auth-ojas-parent/ui-parent/ui-web/src/main/web/src/app/home/admin-role/admin-role.component.spec.ts
@@ -1,1 +1,25 @@
-added admin-role.component.spec.ts
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdminRoleComponent } from './admin-role.component';
+
+describe('AdminRoleComponent', () => {
+  let component: AdminRoleComponent;
+  let fixture: ComponentFixture<AdminRoleComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AdminRoleComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AdminRoleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

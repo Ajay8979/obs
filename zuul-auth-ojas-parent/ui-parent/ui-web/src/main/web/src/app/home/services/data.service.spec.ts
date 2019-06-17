5b82a676b4383ec14b32c5219cfc78c3bb72a594
tshiva <tshiva@host.docker.internal>
2019-06-17 16:16:08 +0530

    added dataservices for employee

--- zuul-auth-ojas-parent/ui-parent/ui-web/src/main/web/src/app/home/services/data.service.spec.ts
+++ zuul-auth-ojas-parent/ui-parent/ui-web/src/main/web/src/app/home/services/data.service.spec.ts
@@ -1,1 +1,12 @@
-added data.service.spec.ts
+import { TestBed } from '@angular/core/testing';
+
+import { DataService } from './data.service';
+
+describe('DataService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: DataService = TestBed.get(DataService);
+    expect(service).toBeTruthy();
+  });
+});

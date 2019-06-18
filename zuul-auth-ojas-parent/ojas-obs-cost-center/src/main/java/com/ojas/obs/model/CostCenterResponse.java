5d9b484de8f94dfab00c24c558e801f205958cf1
tshiva <tshiva@host.docker.internal>
2019-06-18 18:14:27 +0530

    removed total count, pageNo and pagesize

--- zuul-auth-ojas-parent/ojas-obs-cost-center/src/main/java/com/ojas/obs/model/CostCenterResponse.java
+++ zuul-auth-ojas-parent/ojas-obs-cost-center/src/main/java/com/ojas/obs/model/CostCenterResponse.java
@@ -5,49 +5,68 @@ import java.util.List;
 public class CostCenterResponse {
 
 	private List<CostCenter> listOfCostCenter;
-	private String statusMessage;
-	private Integer totalCount;
-	private int pageNo;
-	private int pageSize;
+	private String message;
+	private String statusCode;
 	public List<CostCenter> getListOfCostCenter() {
 		return listOfCostCenter;
 	}
 	public void setListOfCostCenter(List<CostCenter> listOfCostCenter) {
 		this.listOfCostCenter = listOfCostCenter;
 	}
-	public String getStatusMessage() {
-		return statusMessage;
+	public String getMessage() {
+		return message;
 	}
-	public void setStatusMessage(String statusMessage) {
-		this.statusMessage = statusMessage;
+	public void setMessage(String message) {
+		this.message = message;
 	}
-	public Integer getTotalCount() {
-		return totalCount;
+	public String getStatusCode() {
+		return statusCode;
 	}
-	public void setTotalCount(Integer totalCount) {
-		this.totalCount = totalCount;
-	}
-	public int getPageNo() {
-		return pageNo;
-	}
-	public void setPageNo(int pageNo) {
-		this.pageNo = pageNo;
-	}
-	public int getPageSize() {
-		return pageSize;
-	}
-	public void setPageSize(int pageSize) {
-		this.pageSize = pageSize;
-	}
-	public CostCenterResponse() {
-		super();
-		// TODO Auto-generated constructor stub
+	public void setStatusCode(String statusCode) {
+		this.statusCode = statusCode;
 	}
 	@Override
 	public String toString() {
-		return "CostCenterResponse [listOfCostCenter=" + listOfCostCenter + ", statusMessage=" + statusMessage
-				+ ", totalCount=" + totalCount + ", pageNo=" + pageNo + ", pageSize=" + pageSize + "]";
+		return "CostCenterResponse [listOfCostCenter=" + listOfCostCenter + ", message=" + message + ", statusCode="
+				+ statusCode + "]";
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((listOfCostCenter == null) ? 0 : listOfCostCenter.hashCode());
+		result = prime * result + ((message == null) ? 0 : message.hashCode());
+		result = prime * result + ((statusCode == null) ? 0 : statusCode.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CostCenterResponse other = (CostCenterResponse) obj;
+		if (listOfCostCenter == null) {
+			if (other.listOfCostCenter != null)
+				return false;
+		} else if (!listOfCostCenter.equals(other.listOfCostCenter))
+			return false;
+		if (message == null) {
+			if (other.message != null)
+				return false;
+		} else if (!message.equals(other.message))
+			return false;
+		if (statusCode == null) {
+			if (other.statusCode != null)
+				return false;
+		} else if (!statusCode.equals(other.statusCode))
+			return false;
+		return true;
+	}
+	
+	
 	
 	
 }

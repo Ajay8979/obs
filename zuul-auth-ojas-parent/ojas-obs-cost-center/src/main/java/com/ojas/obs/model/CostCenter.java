618fbe25508d258e9655579351a5d033bb1e0763
tshiva <tshiva@host.docker.internal>
2019-06-18 18:13:06 +0530

    added toString method

--- zuul-auth-ojas-parent/ojas-obs-cost-center/src/main/java/com/ojas/obs/model/CostCenter.java
+++ zuul-auth-ojas-parent/ojas-obs-cost-center/src/main/java/com/ojas/obs/model/CostCenter.java
@@ -16,6 +16,37 @@ public class CostCenter {
 	public void setId(Integer id) {
 		this.id = id;
 	}
+	@Override
+	public String toString() {
+		return "CostCenter [costCenterCode=" + costCenterCode + ", id=" + id + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + costCenterCode;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CostCenter other = (CostCenter) obj;
+		if (costCenterCode != other.costCenterCode)
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
 	
 	
 }

df2119c650456156a6c52d405ecbaca5670f1f4f
tshiva <tshiva@host.docker.internal>
2019-06-18 17:03:49 +0530

    changed database table name

--- zuul-auth-ojas-parent/ojas-obs-bankdetails/src/main/java/com/ojas/obs/constants/UserConstants.java
+++ zuul-auth-ojas-parent/ojas-obs-bankdetails/src/main/java/com/ojas/obs/constants/UserConstants.java
@@ -19,9 +19,9 @@ public class UserConstants {
 	
 	//--------------------Bank Details------------------//
 	public static final String SAVEBANKDETAILS = "INSERT INTO obs_bankdetails(bank_account_no,bank_name,bank_city,bank_branch,bank_ifsc_code,bank_account_status,employee_id,Is_active,created_by,created_date,flag) VALUES(?,?,?,?,?,?,?,?,?,?,?)";
-	public static final String DELETEBANKDETAILS = "update obs_BankDetails set flag = ? where id = ?";
-	public static final String UPDATEBANKDETAILS = "Update obs_BankDetails set bank_account_no=?,bank_name=?, bank_city=?,bank_branch=?, bank_ifsc_code=?, bank_account_status=?, Is_active=?,updated_by=?,updated_date=?,flag=? where employee_id = ? ";
-	public static final String RECORDSCOUNT = "select count(*) from obs_BankDetails";
-	public static final String ALLRECORDS = "select * from obs_BankDetails where flag !=false ";
-	public static final String GETBYID = "select * from obs_bankdetails where employee_id  = ?";
+	public static final String DELETEBANKDETAILS = "update obs_bankdetails set flag = ? where id = ?";
+	public static final String UPDATEBANKDETAILS = "Update obs_bankdetails set bank_account_no=?,bank_name=?, bank_city=?,bank_branch=?, bank_ifsc_code=?, bank_account_status=?, Is_active=?,updated_by=?,updated_date=?,flag=? where id = ? ";
+	public static final String RECORDSCOUNT = "select count(*) from obs_bankdetails";
+	public static final String ALLRECORDS = "select * from obs_bankdetails where flag !=false ";
+	public static final String GETBYID = "select * from obs_bankdetails where employee_id  = ? and flag =1";
 }

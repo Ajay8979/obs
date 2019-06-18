b4f5bf44a6016be962651a6f4a44bec6b58f0dd0
tshiva <tshiva@host.docker.internal>
2019-06-18 18:23:10 +0530

    added hashCode and equals methods

--- zuul-auth-ojas-parent/ojas-obs-dependentdetails/src/main/java/com/ojas/obs/model/DependentDetails.java
+++ zuul-auth-ojas-parent/ojas-obs-dependentdetails/src/main/java/com/ojas/obs/model/DependentDetails.java
@@ -18,20 +18,7 @@ public class DependentDetails {
 		
 		public DependentDetails() {}
 
-		public DependentDetails(int id, String dependent_Name, String relation, Date date_Of_Birth, String employee_Id,
-				String created_By, Timestamp created_Date, String updated_By, Timestamp updated_Date, boolean flag) {
-			super();
-			this.id = id;
-			this.dependent_Name = dependent_Name;
-			this.relation = relation;
-			this.date_Of_Birth = date_Of_Birth;
-			this.employee_Id = employee_Id;
-			this.created_By = created_By;
-			this.created_Date = created_Date;
-			this.updated_By = updated_By;
-			this.updated_Date = updated_Date;
-			this.flag = flag;
-		}
+		
 
 		public int getId() {
 			return id;
@@ -111,6 +98,87 @@ public class DependentDetails {
 
 		public void setFlag(boolean flag) {
 			this.flag = flag;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((created_By == null) ? 0 : created_By.hashCode());
+			result = prime * result + ((created_Date == null) ? 0 : created_Date.hashCode());
+			result = prime * result + ((date_Of_Birth == null) ? 0 : date_Of_Birth.hashCode());
+			result = prime * result + ((dependent_Name == null) ? 0 : dependent_Name.hashCode());
+			result = prime * result + ((employee_Id == null) ? 0 : employee_Id.hashCode());
+			result = prime * result + (flag ? 1231 : 1237);
+			result = prime * result + id;
+			result = prime * result + ((relation == null) ? 0 : relation.hashCode());
+			result = prime * result + ((updated_By == null) ? 0 : updated_By.hashCode());
+			result = prime * result + ((updated_Date == null) ? 0 : updated_Date.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			DependentDetails other = (DependentDetails) obj;
+			if (created_By == null) {
+				if (other.created_By != null)
+					return false;
+			} else if (!created_By.equals(other.created_By))
+				return false;
+			if (created_Date == null) {
+				if (other.created_Date != null)
+					return false;
+			} else if (!created_Date.equals(other.created_Date))
+				return false;
+			if (date_Of_Birth == null) {
+				if (other.date_Of_Birth != null)
+					return false;
+			} else if (!date_Of_Birth.equals(other.date_Of_Birth))
+				return false;
+			if (dependent_Name == null) {
+				if (other.dependent_Name != null)
+					return false;
+			} else if (!dependent_Name.equals(other.dependent_Name))
+				return false;
+			if (employee_Id == null) {
+				if (other.employee_Id != null)
+					return false;
+			} else if (!employee_Id.equals(other.employee_Id))
+				return false;
+			if (flag != other.flag)
+				return false;
+			if (id != other.id)
+				return false;
+			if (relation == null) {
+				if (other.relation != null)
+					return false;
+			} else if (!relation.equals(other.relation))
+				return false;
+			if (updated_By == null) {
+				if (other.updated_By != null)
+					return false;
+			} else if (!updated_By.equals(other.updated_By))
+				return false;
+			if (updated_Date == null) {
+				if (other.updated_Date != null)
+					return false;
+			} else if (!updated_Date.equals(other.updated_Date))
+				return false;
+			return true;
+		}
+
+		@Override
+		public String toString() {
+			return "DependentDetails [id=" + id + ", dependent_Name=" + dependent_Name + ", relation=" + relation
+					+ ", date_Of_Birth=" + date_Of_Birth + ", employee_Id=" + employee_Id + ", created_By=" + created_By
+					+ ", created_Date=" + created_Date + ", updated_By=" + updated_By + ", updated_Date=" + updated_Date
+					+ ", flag=" + flag + "]";
 		}
 
 	

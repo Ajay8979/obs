c3c969554aaca04ff47f1c65fb8d8015280cb590
tshiva <tshiva@host.docker.internal>
2019-06-18 18:52:57 +0530

    added sessionId

--- zuul-auth-ojas-parent/ojas-obs-educationtype/src/main/java/com/ojas/obs/modelrequest/EmployeeEducationRequest.java
+++ zuul-auth-ojas-parent/ojas-obs-educationtype/src/main/java/com/ojas/obs/modelrequest/EmployeeEducationRequest.java
@@ -12,18 +12,7 @@ import com.ojas.obs.model.EmployeeEducation;
 public class EmployeeEducationRequest {
 
 	private List<EmployeeEducation> listEmployeeEducations;
-
-	private String sessionID;
-
-	private String searchKey;
-
-	private Integer pageNo;
-
-	private Integer pageSize;
-
-	private String transactionType;
-
-	
+    private String transactionType;
 
 	public List<EmployeeEducation> getListEmployeeEducations() {
 		return listEmployeeEducations;
@@ -32,39 +21,6 @@ public class EmployeeEducationRequest {
 	public void setListEmployeeEducations(List<EmployeeEducation> listEmployeeEducations) {
 		this.listEmployeeEducations = listEmployeeEducations;
 	}
-
-	public String getSessionID() {
-		return sessionID;
-	}
-
-	public void setSessionID(String sessionID) {
-		this.sessionID = sessionID;
-	}
-
-	public String getSearchKey() {
-		return searchKey;
-	}
-
-	public void setSearchKey(String searchKey) {
-		this.searchKey = searchKey;
-	}
-
-	public Integer getPageNo() {
-		return pageNo;
-	}
-
-	public void setPageNo(Integer pageNo) {
-		this.pageNo = pageNo;
-	}
-
-	public Integer getPageSize() {
-		return pageSize;
-	}
-
-	public void setPageSize(Integer pageSize) {
-		this.pageSize = pageSize;
-	}
-
 	public String getTransactionType() {
 		return transactionType;
 	}
@@ -72,16 +28,19 @@ public class EmployeeEducationRequest {
 	public void setTransactionType(String transactionType) {
 		this.transactionType = transactionType;
 	}
+	@Override
+	public String toString() {
+		return "EmployeeEducationRequest [listEmployeeEducations=" + listEmployeeEducations + ", transactionType="
+				+ transactionType + "]";
+	}
+	
+
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((listEmployeeEducations == null) ? 0 : listEmployeeEducations.hashCode());
-		result = prime * result + ((pageNo == null) ? 0 : pageNo.hashCode());
-		result = prime * result + ((pageSize == null) ? 0 : pageSize.hashCode());
-		result = prime * result + ((searchKey == null) ? 0 : searchKey.hashCode());
-		result = prime * result + ((sessionID == null) ? 0 : sessionID.hashCode());
 		result = prime * result + ((transactionType == null) ? 0 : transactionType.hashCode());
 		return result;
 	}
@@ -100,26 +59,6 @@ public class EmployeeEducationRequest {
 				return false;
 		} else if (!listEmployeeEducations.equals(other.listEmployeeEducations))
 			return false;
-		if (pageNo == null) {
-			if (other.pageNo != null)
-				return false;
-		} else if (!pageNo.equals(other.pageNo))
-			return false;
-		if (pageSize == null) {
-			if (other.pageSize != null)
-				return false;
-		} else if (!pageSize.equals(other.pageSize))
-			return false;
-		if (searchKey == null) {
-			if (other.searchKey != null)
-				return false;
-		} else if (!searchKey.equals(other.searchKey))
-			return false;
-		if (sessionID == null) {
-			if (other.sessionID != null)
-				return false;
-		} else if (!sessionID.equals(other.sessionID))
-			return false;
 		if (transactionType == null) {
 			if (other.transactionType != null)
 				return false;
@@ -127,5 +66,10 @@ public class EmployeeEducationRequest {
 			return false;
 		return true;
 	}
+
+	
+	
+
+	
 
 }

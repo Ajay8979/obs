eccd1e9e5755777e9c0231b3e7b3e7a006f1360a
tshiva <tshiva@host.docker.internal>
2019-06-18 17:32:43 +0530

    added delete sql queries

--- zuul-auth-ojas-parent/ojas-obs-cost-center/src/main/java/com/ojas/obs/constants/UserConstants.java
+++ zuul-auth-ojas-parent/ojas-obs-cost-center/src/main/java/com/ojas/obs/constants/UserConstants.java
@@ -5,7 +5,7 @@ public class UserConstants {
 
 	public static final String INSERTCOSTCENTER = "insert into obs_costcenter(costcentercode) values (?)";
 	public static final String UPDATECOSTCENTER = "update obs_costcenter set costcentercode= ? where id= ?";
-	public static final String DELETECOSTCENTER = "delete obs_costcenter where id= ?";
+	public static final String DELETECOSTCENTER = "delete from obs_costcenter where id= ?";
 	public static final String GETCOSTCENTER = "select * from obs_costcenter";
 	public static final String GETCOSTCENTERCOUNT ="select count(*) from obs_costcenter";
 

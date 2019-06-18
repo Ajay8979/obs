61740ea2f9e69353d534fa5ea784e1a97f76565e
tshiva <tshiva@host.docker.internal>
2019-06-18 14:57:44 +0530

    added sql exception

--- zuul-auth-ojas-parent/ojas-obs-subbusinessunit/src/main/java/com/ojas/obs/facade/SubBusinessUnitFacade.java
+++ zuul-auth-ojas-parent/ojas-obs-subbusinessunit/src/main/java/com/ojas/obs/facade/SubBusinessUnitFacade.java
@@ -1,11 +1,13 @@
 package com.ojas.obs.facade;
 
+import java.sql.SQLException;
+
 import org.springframework.http.ResponseEntity;
 
 import com.ojas.obs.request.SubBusinessUnitRequest;
 
 public interface SubBusinessUnitFacade {
-	ResponseEntity<Object> setSubBusinessUnit(SubBusinessUnitRequest subBusinessUnitRequest);
-	ResponseEntity<Object> getSubBusinessUnit(SubBusinessUnitRequest subBusinessUnitRequest);
+	ResponseEntity<Object> setSubBusinessUnit(SubBusinessUnitRequest subBusinessUnitRequest)throws SQLException;
+	ResponseEntity<Object> getSubBusinessUnit(SubBusinessUnitRequest subBusinessUnitRequest)throws SQLException;
 
 }

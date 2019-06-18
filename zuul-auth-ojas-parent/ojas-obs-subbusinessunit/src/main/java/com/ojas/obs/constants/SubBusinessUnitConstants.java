811be7bf66d2351c6bb9a010947e3c6ca9e68d45
tshiva <tshiva@host.docker.internal>
2019-06-18 14:44:22 +0530

    added constants for database quries

--- zuul-auth-ojas-parent/ojas-obs-subbusinessunit/src/main/java/com/ojas/obs/constants/SubBusinessUnitConstants.java
+++ zuul-auth-ojas-parent/ojas-obs-subbusinessunit/src/main/java/com/ojas/obs/constants/SubBusinessUnitConstants.java
@@ -11,5 +11,11 @@ public class SubBusinessUnitConstants {
 	public static final String UPDATE = "update";
 	public static final String FAILED = "falied";
 	public static final String SUCCESS = "success";
+	
+	public static final String SAVEBUSINESSUNIT = "Insert into obs_subbusinessunit(name,businessUnitId,CostCenterId) VALUES (?,?,?)";
+	public static final String UPDATEBUSINESSUNITID = "update obs_subbusinessunit set name = ?, businessUnitId = ?, CostCenterId = ? where id = ? ";
+	public static final String GETALLSUBBUSINESSUNITS = "select sbu.*, bu.businessUnitName from obs_subbusinessunit sbu, obs_businessunit bu WHERE bu.id = sbu.businessUnitId";
+	public static final String GETBYIDBUSINESSUNITS = "select * from obs_subbusinessunit where id = ?";
+	
 
 }

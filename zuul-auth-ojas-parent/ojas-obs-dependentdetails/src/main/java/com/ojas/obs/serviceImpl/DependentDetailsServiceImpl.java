68c3b44b09c76bcefe832b6983b9db4d790b5730
tshiva <tshiva@host.docker.internal>
2019-06-18 18:26:36 +0530

    added proper logger messages

--- zuul-auth-ojas-parent/ojas-obs-dependentdetails/src/main/java/com/ojas/obs/serviceImpl/DependentDetailsServiceImpl.java
+++ zuul-auth-ojas-parent/ojas-obs-dependentdetails/src/main/java/com/ojas/obs/serviceImpl/DependentDetailsServiceImpl.java
@@ -39,6 +39,7 @@ public class DependentDetailsServiceImpl implements DependentDetailsService {
 	public DependentDetailsResponse setDependentDetails(
 			DependentDetailsRequest dependentDetailsRequestObject)throws SQLException {
 		List<DependentDetails> dependentDetails = dependentDetailsRequestObject.getDependentDetails();
+		logger.debug("The request details in Service set method: " + dependentDetailsRequestObject);
 		dependentDetailsResponse = new DependentDetailsResponse();
 		if (dependentDetailsRequestObject.getTransactionType().equalsIgnoreCase(SAVE)) {
 			boolean b=false;
@@ -50,9 +51,9 @@ public class DependentDetailsServiceImpl implements DependentDetailsService {
 					|| ((details.getDate_Of_Birth() == null))
 					|| ((details.getEmployee_Id() == null||details.getEmployee_Id().isEmpty())
 					|| ((details.getCreated_By() == null||details.getCreated_By().isEmpty())))) {
-                 
+				logger.debug("data is  invalid "+dependentDetailsRequestObject );
 				dependentDetailsResponse.setStatusCode("422");
-				dependentDetailsResponse.setStatusMessage("Improper data");
+				dependentDetailsResponse.setMessage("Improper data");
 				b=true; 
 				break;
 			}
@@ -60,10 +61,11 @@ public class DependentDetailsServiceImpl implements DependentDetailsService {
 			if(b) {}
 			else {
 				int saveDependentDetails = dependentDetailsDaoImpl.saveDependentDetails(dependentDetailsRequestObject);
-				logger.debug("received at service by calling the saveDependentDetails is" + saveDependentDetails);
+				
 				if (saveDependentDetails != 0) {
 					dependentDetailsResponse.setStatusCode("200");
-					dependentDetailsResponse.setStatusMessage("DependentDetails have saved successfully");
+					logger.debug("inside  save method Success fully record added.****** : " + saveDependentDetails);
+					dependentDetailsResponse.setMessage("DependentDetails have saved successfully");
 				}
 			}
 			return dependentDetailsResponse;
@@ -79,9 +81,9 @@ public class DependentDetailsServiceImpl implements DependentDetailsService {
 								|| ((details.getRelation() == null)|| (details.getRelation().equalsIgnoreCase("")))
 								|| ((details.getDate_Of_Birth() == null))|| ((details.getEmployee_Id() == null||details.getEmployee_Id().isEmpty())
 								|| ((details.getUpdated_By() == null ||details.getUpdated_By().isEmpty())))) {
-	
+							logger.debug("data is  invalid "+dependentDetailsRequestObject );
 								dependentDetailsResponse.setStatusCode("422");
-								dependentDetailsResponse.setStatusMessage("Improper data");
+								dependentDetailsResponse.setMessage("Improper data");
 								b=true;
 								
 						}
@@ -91,11 +93,13 @@ public class DependentDetailsServiceImpl implements DependentDetailsService {
 			else {
 				int updateDependentDetails = dependentDetailsDaoImpl.updateDependentDetails(dependentDetailsRequestObject);
 				dependentDetailsResponse.setStatusCode("409");
-				dependentDetailsResponse.setStatusMessage("sorry DependentDetails are not updated");
-				logger.debug("received at service by calling the updateDependentDetails is" + updateDependentDetails);
+				dependentDetailsResponse.setMessage("sorry DependentDetails are not updated");
+				
 						if (updateDependentDetails != 0) {
 							dependentDetailsResponse.setStatusCode("200");
-							dependentDetailsResponse.setStatusMessage("DependentDetails are updated successfully");
+							logger.debug("inside  setMethod Success fully record Updated.****** : " + updateDependentDetails);
+							dependentDetailsResponse.setMessage("DependentDetails are updated successfully");
+							
 						}
 			}
 				
@@ -107,7 +111,8 @@ public class DependentDetailsServiceImpl implements DependentDetailsService {
 			for(DependentDetails details : dependentDetails) {
 				if (0 == details.getId()) {
 					dependentDetailsResponse.setStatusCode("422");
-					dependentDetailsResponse.setStatusMessage("please provide the id");
+					logger.debug("Please provid the id :" +details.getId());
+					dependentDetailsResponse.setMessage("please provide the id");
 					b= true;
 
 				}
@@ -116,16 +121,19 @@ public class DependentDetailsServiceImpl implements DependentDetailsService {
 			else {
 					int deleteDependentDetails = dependentDetailsDaoImpl.deleteDependentDetails(dependentDetailsRequestObject);
 					dependentDetailsResponse.setStatusCode("409");
-					dependentDetailsResponse.setStatusMessage("sorry DependentDetails are not updated");
-					logger.debug("received at service by calling the deleteDependentDetails is" + deleteDependentDetails);
+					dependentDetailsResponse.setMessage("sorry DependentDetails are not updated");
+					
 						if (deleteDependentDetails != 0) {
 							dependentDetailsResponse.setStatusCode("200");
-							dependentDetailsResponse.setStatusMessage("DependentDetails are deleted successfully");
+							logger.debug("inside  setMethod Success fully record Deleted.****** : " + deleteDependentDetails);
+							dependentDetailsResponse.setMessage("DependentDetails are deleted successfully");
 						}
 				}
 			}	
 			else {
-				dependentDetailsResponse.setStatusMessage("please provide the id");
+				dependentDetailsResponse.setMessage("please provide the id");
+				logger.debug("Please provid the id ");
+				dependentDetailsResponse.setStatusCode("409");
 			}	
 			return dependentDetailsResponse;
 	}
@@ -138,47 +146,55 @@ public class DependentDetailsServiceImpl implements DependentDetailsService {
 	public DependentDetailsResponse getDependentDetails(DependentDetailsRequest dependentDetailsRequestObject)
 			throws SQLException {
 		dependentDetailsResponse = new DependentDetailsResponse();
+		logger.debug("The request details in Service get method: " + dependentDetailsRequestObject);
 		if (dependentDetailsRequestObject.getTransactionType().equalsIgnoreCase(GETALL)) {
 			List<DependentDetails> allDependentDetails = dependentDetailsDaoImpl.getAll(dependentDetailsRequestObject);
-			logger.debug("received at service by calling the getAllmethod is" + allDependentDetails);
+			
 			if (!allDependentDetails.isEmpty()) {
 				dependentDetailsResponse.setStatusCode("200");
-				dependentDetailsResponse.setStatusMessage("you got the list of DependentDetails successfully");
+				logger.debug("you got the list of DependentDetails successfully" + allDependentDetails);
+				dependentDetailsResponse.setMessage("you got the list of DependentDetails successfully");
 				dependentDetailsResponse.setGetDependentDetailsList(allDependentDetails);;
 			} else {
 				dependentDetailsResponse.setGetDependentDetailsList(allDependentDetails);
 				dependentDetailsResponse.setStatusCode("200");
-				dependentDetailsResponse.setStatusMessage("no records found");
+				logger.debug("no records found: " + allDependentDetails);
+				dependentDetailsResponse.setMessage("no records found");
 			}
 		}
 		
 		else if (dependentDetailsRequestObject.getTransactionType().equalsIgnoreCase(GETBYID)) {
 			List<DependentDetails> all = null;
-			logger.debug("received at service by calling the getAllmethod is" + all);
+			
 			String EmpId=dependentDetailsRequestObject.getDependentDetails().get(0).getEmployee_Id();
 			if(EmpId == null) {
 				all = dependentDetailsDaoImpl.getById(dependentDetailsRequestObject.getDependentDetails().get(0).getId());
+				logger.debug("you got the list of DependentDetails successfully Using id: " + all);
 			}
 			else {
 				all = dependentDetailsDaoImpl.getByEmpId(dependentDetailsRequestObject.getDependentDetails().get(0).getEmployee_Id());
+				logger.debug("you got the list of DependentDetails successfully Using employee_id:  " + all);
 			
 			}
 			
 			
 			if (!all.isEmpty()) {
 				dependentDetailsResponse.setStatusCode("200");
-				dependentDetailsResponse.setStatusMessage("you got the list of DependentDetails successfully");
+				logger.debug("you got the list of DependentDetails successfully" + all);
+				dependentDetailsResponse.setMessage("you got the list of DependentDetails successfully");
 				dependentDetailsResponse.setGetDependentDetailsList(all);;
 			} else {
 				dependentDetailsResponse.setGetDependentDetailsList(all);
 				dependentDetailsResponse.setStatusCode("200");
-				dependentDetailsResponse.setStatusMessage("no records found");
+				logger.debug("No records Found " + all);
+				dependentDetailsResponse.setMessage("no records found");
 			}
 		}
 		
 		else {
 			dependentDetailsResponse.setStatusCode("422");
-			dependentDetailsResponse.setStatusMessage("please provide valid transaction type");
+			logger.debug("Please provide valid transactionType: "+dependentDetailsRequestObject.getTransactionType() );
+			dependentDetailsResponse.setMessage("please provide valid transaction type");
 		}
 		
 		return dependentDetailsResponse;

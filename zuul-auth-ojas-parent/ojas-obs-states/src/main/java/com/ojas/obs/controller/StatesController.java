852e819983e9a161b5719e1554a47e28f8eb75a2
tshiva <tshiva@host.docker.internal>
2019-06-18 15:11:26 +0530

    added proper log messages and removed session

--- zuul-auth-ojas-parent/ojas-obs-states/src/main/java/com/ojas/obs/controller/StatesController.java
+++ zuul-auth-ojas-parent/ojas-obs-states/src/main/java/com/ojas/obs/controller/StatesController.java
@@ -3,7 +3,7 @@ import static com.ojas.obs.constants.Constants.DELETE;
 import static com.ojas.obs.constants.Constants.GET;
 import static com.ojas.obs.constants.Constants.SET;
 import static com.ojas.obs.constants.Constants.UPDATE;
-//import static com.ojas.obs.constants.Constants.STATES;
+
 import java.sql.SQLException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -26,7 +26,7 @@ import com.ojas.obs.request.StatesRequest;
  *
  */
 @RestController
-//@RequestMapping(STATES)
+//@RequestMapping("/obs/states")
 public class StatesController {
 
 	@Autowired
@@ -36,28 +36,19 @@ public class StatesController {
 	@PostMapping(SET)
 	public ResponseEntity<Object> setStates(@RequestBody StatesRequest statesRequestObj,
 			HttpServletRequest servletRequest, HttpServletResponse servletResponse){
-		String sessionId = null;
-		logger.debug("incoming requests " + statesRequestObj);
+		logger.info("@@@@Inside setStates " + statesRequestObj);
 		
 		try {
 			if (statesRequestObj == null) {
+				logger.debug("@@@@Inside Requst Object is null " + statesRequestObj);
 				ErrorResponse error = new ErrorResponse();
 				logger.debug("request is not valid");
 				error.setMessage("statesRequestObj is not valid");
 				error.setStatusCode("422");
 				return new ResponseEntity<Object>(error, HttpStatus.UNPROCESSABLE_ENTITY);
-			} else {
-				sessionId = statesRequestObj.getSessionId();
-				logger.debug("incoming request " + sessionId);
-			}
-			if (statesRequestObj.getSessionId() == null) {
-				ErrorResponse error = new ErrorResponse();
-				logger.debug("session id check");
-				error.setMessage("session id must be provided");
-				error.setStatusCode("422");
-				return new ResponseEntity<Object>(error, HttpStatus.UNPROCESSABLE_ENTITY);
-			}
+			} 
 			if (statesRequestObj.getTransactionType() == null) {
+				logger.debug("@@@@Inside setStates TransactionType is null ");
 				ErrorResponse error = new ErrorResponse();
 				logger.debug("transaction type check");
 				error.setMessage("transactionType must be provided");
@@ -66,6 +57,7 @@ public class StatesController {
 			}
 			for(States states: statesRequestObj.getStates()) {
 					if (null ==states.getStateName()|| states.getStateName().isEmpty()) {
+						logger.debug("@@@@Inside setStates State Name is null ");
 						ErrorResponse error = new ErrorResponse();
 						error.setMessage("states can't be null or empty");
 						error.setStatusCode("422");
@@ -74,9 +66,9 @@ public class StatesController {
 			}
 			for(States states: statesRequestObj.getStates()) {
 				if ((statesRequestObj.getTransactionType().equalsIgnoreCase(UPDATE)|| statesRequestObj.getTransactionType().equalsIgnoreCase(DELETE))&& 0 == states.getId()) {
-				ErrorResponse error = new ErrorResponse();
-				logger.debug("data is  invalid");
-				error.setMessage("Request is  invalid");
+				logger.debug("@@@@Inside setStates id is null ");
+			    ErrorResponse error = new ErrorResponse();
+				error.setMessage("Please provide id");
 				error.setStatusCode("422");
 				return new ResponseEntity<>(error, HttpStatus.UNPROCESSABLE_ENTITY);
 			}
@@ -85,26 +77,30 @@ public class StatesController {
             return statesFacade.setStates(statesRequestObj);
 
 		}catch (DuplicateKeyException exception) {
-			logger.debug("inside catch block.*******");
+			System.out.println("StatesController.setStates()@@@@DuplicateKeyException");
+			logger.error("@@@@Inside DuplicateKeyException catch block.");
 			ErrorResponse error = new ErrorResponse();
-			error.setStatusMessage(exception.getCause().getLocalizedMessage());
-			//error.setStatusCode("409");
+			error.setStatusMessage(exception.getCause().getMessage());
+			error.setMessage("duplicates are not allowed.");
+			error.setStatusCode("409");
 			exception.printStackTrace();
 			return new ResponseEntity<>(error, HttpStatus.CONFLICT);
 	  }
 		catch (SQLException exception) {
-			logger.debug("inside catch SQLblock.*******");
+			logger.error("@@@@Inside SQLException catch SQLblock.");
 			ErrorResponse error = new ErrorResponse();
 			error.setStatusMessage(exception.getMessage());
-			//error.setStatusCode("409");
+			error.setStatusCode("409");
+			error.setMessage("SQLException");
 			exception.printStackTrace();
 			return new ResponseEntity<>(error, HttpStatus.CONFLICT);
 	  }
 		catch (Exception exception) {
-			logger.debug("inside catch Exception block.*******");
+			logger.error("@@@@Inside Exception catch Exception block.");
 			ErrorResponse error = new ErrorResponse();
 			error.setStatusMessage(exception.getMessage());
-			//error.setStatusCode("409");
+			error.setMessage("Exception");
+			error.setStatusCode("409");
 			exception.printStackTrace();
 			return new ResponseEntity<>(error, HttpStatus.CONFLICT);
 	  }
@@ -112,26 +108,31 @@ public class StatesController {
 
 	@PostMapping(GET)
 	public ResponseEntity<Object> getStates(@RequestBody StatesRequest statesRequestObj) {
-        String sessionId = null;
+		logger.info("@@@@Inside getStates " + statesRequestObj);
+        
 		try {
-			logger.debug("requestObject received = " + statesRequestObj);
 			if (statesRequestObj == null) {
+				logger.debug("@@@@Inside getStates statesRequestObj is null");
 				ErrorResponse error = new ErrorResponse();
 				error.setMessage("statesRequestObj is not valid");
 				error.setStatusCode("422");
 				return new ResponseEntity<Object>(error, HttpStatus.UNPROCESSABLE_ENTITY);
-			} else {
-				sessionId = statesRequestObj.getSessionId();
-				logger.debug("incoming request " + sessionId);
-			}
-			return statesFacade.getStates(statesRequestObj);
+			} 
+		return statesFacade.getStates(statesRequestObj);
 		} catch (SQLException exception) {
+			logger.error("@@@@Inside SQLException catch block");
 			ErrorResponse error = new ErrorResponse();
+			error.setMessage("SQLException");
+			error.setStatusCode("409");
 			error.setStatusMessage(exception.getMessage());
 			return new ResponseEntity<>(error, HttpStatus.CONFLICT);
 		}
 		catch (Exception exception) {
+			logger.error("@@@@Inside Exception catch block");
 			ErrorResponse error = new ErrorResponse();
+			error.setMessage("Exception");
+			error.setStatusCode("409");
+			//exception.printStackTrace();
 			error.setStatusMessage(exception.getMessage());
 			return new ResponseEntity<>(error, HttpStatus.CONFLICT);
 		}

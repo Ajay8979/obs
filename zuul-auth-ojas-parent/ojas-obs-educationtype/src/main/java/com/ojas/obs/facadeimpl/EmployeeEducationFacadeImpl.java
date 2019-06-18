19928be53e02abe4d0e1aa86b7359ae3943f788f
tshiva <tshiva@host.docker.internal>
2019-06-18 18:51:34 +0530

    added proper logger messages

--- zuul-auth-ojas-parent/ojas-obs-educationtype/src/main/java/com/ojas/obs/facadeimpl/EmployeeEducationFacadeImpl.java
+++ zuul-auth-ojas-parent/ojas-obs-educationtype/src/main/java/com/ojas/obs/facadeimpl/EmployeeEducationFacadeImpl.java
@@ -1,18 +1,17 @@
 package com.ojas.obs.facadeimpl;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.ojas.obs.constants.UserConstants;
 import com.ojas.obs.dao.EmployeeEducationDao;
-import com.ojas.obs.error.ErrorResponse;
 import com.ojas.obs.facade.EmployeeEducationFacade;
 import com.ojas.obs.model.EmployeeEducation;
 import com.ojas.obs.modelrequest.EmployeeEducationRequest;
@@ -39,68 +38,60 @@ public class EmployeeEducationFacadeImpl implements EmployeeEducationFacade {
 	 */
 
 	@Override
-	public ResponseEntity<Object> setEmployeeEducationInfo(EmployeeEducationRequest employeeEducationRequest) {
+	public ResponseEntity<Object> setEmployeeEducationInfo(EmployeeEducationRequest employeeEducationRequest)
+			throws SQLException {
 		EmployeeEducationResponse employeeEducationResponse = null;
-		try {
-			if (employeeEducationRequest.getTransactionType().equalsIgnoreCase(UserConstants.SAVE)) {
-				employeeEducationResponse = new EmployeeEducationResponse();
-				boolean employeeEducation = employeeEducationDao.saveEmployeeEducation(employeeEducationRequest);
-				if (employeeEducation) {
-					int allRecordsCount = employeeEducationDao.getAllRecordsCount();
-					employeeEducationResponse.setTotalCount(allRecordsCount);
-					logger.debug("the response for request is ready");
-					logger.debug("inside the save method" + employeeEducation);
-					employeeEducationResponse.setStatusMessage("Successfully employeee education record saved");
-					return new ResponseEntity<>(employeeEducationResponse, HttpStatus.OK);
-				} else {
-					employeeEducationResponse.setStatusMessage("FAILED");
-					logger.debug("request not valid");
-					return new ResponseEntity<>(employeeEducationResponse, HttpStatus.CONFLICT);
-				}
-			}
-			if (employeeEducationRequest.getTransactionType().equalsIgnoreCase(UserConstants.DELETE)) {
-				employeeEducationResponse = new EmployeeEducationResponse();
-				boolean isDeleted = false;
-				List<EmployeeEducation> employeeEducations = employeeEducationRequest.getListEmployeeEducations();
-				for (EmployeeEducation employeeEducation : employeeEducations) {
-					int id = employeeEducation.getId();
-					isDeleted = employeeEducationDao.deleteEmployeeEducation(id);
-				}
-				if (isDeleted) {
-					int allRecordsCount = employeeEducationDao.getAllRecordsCount();
-					employeeEducationResponse.setTotalCount(allRecordsCount);
-					employeeEducationResponse.setStatusMessage("Successfully employeee education deleted");
-					return new ResponseEntity<>(employeeEducationResponse, HttpStatus.OK);
-				} else {
-					employeeEducationResponse.setStatusMessage("FAILED");
-					logger.debug("request not valid");
-					return new ResponseEntity<>(employeeEducationResponse, HttpStatus.CONFLICT);
-				}
+		logger.info("The request details in service set method" + employeeEducationRequest);
+
+		if (employeeEducationRequest.getTransactionType().equalsIgnoreCase(UserConstants.SAVE)) {
+			employeeEducationResponse = new EmployeeEducationResponse();
+			boolean employeeEducation = employeeEducationDao.saveEmployeeEducation(employeeEducationRequest);
+			if (employeeEducation) {
+				int allRecordsCount = employeeEducationDao.getAllRecordsCount();
+				logger.debug("The record count being saved " + allRecordsCount);
+				logger.debug("Details saved " + employeeEducation);
+				employeeEducationResponse.setMessage("Successfully employeee education record saved");
+				return new ResponseEntity<>(employeeEducationResponse, HttpStatus.OK);
+			} else {
+				employeeEducationResponse.setMessage("FAILED");
+				logger.error("request details not saved");
+				return new ResponseEntity<>(employeeEducationResponse, HttpStatus.CONFLICT);
 
 			}
-			if (employeeEducationRequest.getTransactionType().equalsIgnoreCase(UserConstants.UPDATE)) {
-				employeeEducationResponse = new EmployeeEducationResponse();
-				boolean employeeEducation = employeeEducationDao.updateEmployeeEducation(employeeEducationRequest);
-				if (employeeEducation) {
-					int allRecordsCount = employeeEducationDao.getAllRecordsCount();
-					employeeEducationResponse.setTotalCount(allRecordsCount);
-					logger.debug("succesfully employeeEducation updated");
-					employeeEducationResponse.setStatusMessage("succesfully updated");
-					return new ResponseEntity<Object>(employeeEducationResponse, HttpStatus.OK);
-				} else {
-					employeeEducationResponse.setStatusMessage("FAILED");
-					logger.debug("request not valid");
-					return new ResponseEntity<Object>(employeeEducationResponse, HttpStatus.CONFLICT);
-				}
+		}
+		if (employeeEducationRequest.getTransactionType().equalsIgnoreCase(UserConstants.DELETE)) {
+			employeeEducationResponse = new EmployeeEducationResponse();
+			boolean isDeleted = false;
+			List<EmployeeEducation> employeeEducations = employeeEducationRequest.getListEmployeeEducations();
+			for (EmployeeEducation employeeEducation : employeeEducations) {
+				int id = employeeEducation.getId();
+				isDeleted = employeeEducationDao.deleteEmployeeEducation(id);
+			}
+			if (isDeleted) {
+				int allRecordsCount = employeeEducationDao.getAllRecordsCount();
+				logger.debug("The employee education record deleted " + allRecordsCount);
+				employeeEducationResponse.setMessage("Successfully employeee education deleted " + allRecordsCount);
+				return new ResponseEntity<>(employeeEducationResponse, HttpStatus.OK);
+			} else {
+				employeeEducationResponse.setMessage("FAILED");
+				logger.error("Requested object is not deleted");
+				return new ResponseEntity<>(employeeEducationResponse, HttpStatus.CONFLICT);
 			}
 
-		} catch (Exception e) {
-			logger.debug("inside facade catch block ");
-			ErrorResponse error = new ErrorResponse();
-			e.printStackTrace();
-			error.setMessage("educationType object is null");
-			error.setStatusCode("422");
-			return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+		}
+		if (employeeEducationRequest.getTransactionType().equalsIgnoreCase(UserConstants.UPDATE)) {
+			employeeEducationResponse = new EmployeeEducationResponse();
+			boolean employeeEducation = employeeEducationDao.updateEmployeeEducation(employeeEducationRequest);
+			if (employeeEducation) {
+				int allRecordsCount = employeeEducationDao.getAllRecordsCount();
+				logger.debug("employeeEducation details updated");
+				employeeEducationResponse.setMessage("succesfully updated");
+				return new ResponseEntity<Object>(employeeEducationResponse, HttpStatus.OK);
+			} else {
+				employeeEducationResponse.setMessage("FAILED");
+				logger.error("employeeEducation details not updated");
+				return new ResponseEntity<Object>(employeeEducationResponse, HttpStatus.CONFLICT);
+			}
 		}
 
 		return new ResponseEntity<Object>(employeeEducationResponse, HttpStatus.CONFLICT);
@@ -114,37 +105,43 @@ public class EmployeeEducationFacadeImpl implements EmployeeEducationFacade {
 	 */
 
 	@Override
-	public ResponseEntity<Object> getEmployeeEducationInfo(EmployeeEducationRequest employeeEducationRequest) {
+	public ResponseEntity<Object> getEmployeeEducationInfo(EmployeeEducationRequest employeeEducationRequest)
+			throws SQLException {
 		EmployeeEducationResponse employeeEducationResponse = null;
-		try {
-			if (employeeEducationRequest.getTransactionType().equalsIgnoreCase(UserConstants.GETALL)) {
+		logger.info("The request details in service get method" + employeeEducationRequest);
+
+		if (employeeEducationRequest.getTransactionType().equalsIgnoreCase(UserConstants.GETALL)) {
+			if (null != employeeEducationRequest.getListEmployeeEducations().get(0).getId()) {
+				List<EmployeeEducation> byId = employeeEducationDao.getAllEmployeeEducation(employeeEducationRequest);
+				if (null != byId) {
+					employeeEducationResponse = new EmployeeEducationResponse();
+					employeeEducationResponse.setListCourse(byId);
+					employeeEducationResponse.setMessage("successfully got all records");
+					logger.debug("Succesfully retrieved requested record using " + byId);
+					return new ResponseEntity<Object>(employeeEducationResponse, HttpStatus.OK);
+				} else {
+					employeeEducationResponse.setMessage("Failed");
+					logger.error("The request record using" + byId + " not retrieved");
+					return new ResponseEntity<Object>(employeeEducationResponse, HttpStatus.CONFLICT);
+				}
+			} else {
 				List<EmployeeEducation> emplList = employeeEducationDao
 						.getAllEmployeeEducation(employeeEducationRequest);
 
 				employeeEducationResponse = new EmployeeEducationResponse();
 
 				if (null != emplList) {
-
-					int allRecordsCount = employeeEducationDao.getAllRecordsCount();
 					employeeEducationResponse.setListCourse(emplList);
-					employeeEducationResponse.setStatusMessage("successfully got all records");
-					employeeEducationResponse.setTotalCount(allRecordsCount);
+					employeeEducationResponse.setMessage("successfully got all records");
+					logger.debug("List of employee records retrieved " + emplList);
 					return new ResponseEntity<Object>(employeeEducationResponse, HttpStatus.OK);
 				} else {
-					employeeEducationResponse.setStatusMessage("Failed");
+					employeeEducationResponse.setMessage("Failed");
+					logger.error("List of employee records not retrieved");
 					return new ResponseEntity<Object>(employeeEducationResponse, HttpStatus.CONFLICT);
 				}
 			}
-
-		} catch (Exception e) {
-			logger.debug("inside facade catch block ");
-			ErrorResponse error = new ErrorResponse();
-			e.printStackTrace();
-			error.setMessage("educationType object is null");
-			error.setStatusCode("422");
-			return new ResponseEntity<>(error, HttpStatus.CONFLICT);
 		}
-
 		return new ResponseEntity<Object>(employeeEducationResponse, HttpStatus.CONFLICT);
 	}
 

d8c3ec62ba9620d5e73452b6323a710d3c45643e
tshiva <tshiva@host.docker.internal>
2019-06-18 15:13:57 +0530

    added proper log messages

--- zuul-auth-ojas-parent/ojas-obs-states/src/main/java/com/ojas/obs/facade/StatesFacadeImpl.java
+++ zuul-auth-ojas-parent/ojas-obs-states/src/main/java/com/ojas/obs/facade/StatesFacadeImpl.java
@@ -1,5 +1,11 @@
 package com.ojas.obs.facade;
 
+import static com.ojas.obs.constants.Constants.FAILED;
+import static com.ojas.obs.constants.Constants.GETALL;
+import static com.ojas.obs.constants.Constants.GETBYID;
+import static com.ojas.obs.constants.Constants.SAVE;
+import static com.ojas.obs.constants.Constants.UPDATE;
+
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,17 +15,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
 import com.ojas.obs.dao.StatesDao;
-import com.ojas.obs.model.ErrorResponse;
 import com.ojas.obs.model.States;
 import com.ojas.obs.request.StatesRequest;
 import com.ojas.obs.response.StatesResponse;
-import static com.ojas.obs.constants.Constants.DELETE;
-import static com.ojas.obs.constants.Constants.FAILED;
-import static com.ojas.obs.constants.Constants.SAVE;
-import static com.ojas.obs.constants.Constants.UPDATE;
-import static com.ojas.obs.constants.Constants.GETALL;
-import static com.ojas.obs.constants.Constants.GETBYID;
 
 @Service
 public class StatesFacadeImpl implements StatesFacade {
@@ -29,100 +29,74 @@ public class StatesFacadeImpl implements StatesFacade {
 	Logger logger = Logger.getLogger(this.getClass());
 
     @Override
-	public ResponseEntity<Object> setStates(StatesRequest statesRequestObj) {
-
-		logger.debug("inside set method : " + statesRequestObj);
+	public ResponseEntity<Object> setStates(StatesRequest statesRequestObj) throws SQLException {
+        logger.info("@@@@Inside setStates facade : " + statesRequestObj);
 		StatesResponse statesResponse = null;
-		try {
 			if (statesRequestObj.getTransactionType().equalsIgnoreCase(SAVE)) {
 				statesResponse = new StatesResponse();
-				int count = statesDao.getAllStatesCount();
 				boolean savestate = statesDao.saveStates(statesRequestObj);
 				if (savestate) {
-					logger.debug("inside  save condition.****** : " + savestate);
-					statesResponse.setStatusMessage("Successfully record added");
-					statesResponse.setTotalCount(count);
+					logger.debug("@@@@Inside save transaction facade : " + savestate);
+					statesResponse.setMessage("Successfully record added");
 					return new ResponseEntity<>(statesResponse, HttpStatus.OK);
 				} else {
-					statesResponse.setStatusMessage(FAILED);
+					logger.debug("@@@@Inside save transaction failed facade : " + savestate);
+					statesResponse.setMessage(FAILED);
 					return new ResponseEntity<>(statesResponse, HttpStatus.CONFLICT);
 				}
 			}
 			if (statesRequestObj.getTransactionType().equalsIgnoreCase(UPDATE)) {
+				logger.debug("@@@@Inside update transaction condition.");
 				statesResponse = new StatesResponse();
-				int count = statesDao.getAllStatesCount();
 				boolean updateState = statesDao.updateStates(statesRequestObj);
-				logger.debug("inside  update condition.****** : " + updateState);
+				
 				if (updateState) {
-					statesResponse.setStatusMessage("Successfully record updated");
-					statesResponse.setTotalCount(count);
+					logger.debug("@@@@Inside update transaction condition::Successfully record updated");
+					statesResponse.setMessage("Successfully record updated");
+					
 					return new ResponseEntity<>(statesResponse, HttpStatus.OK);
 				} else {
-					statesResponse.setStatusMessage(FAILED);
+					logger.debug("@@@@Inside update transaction condition:: record failed to update");
+					statesResponse.setMessage(FAILED);
 					return new ResponseEntity<>(statesResponse, HttpStatus.CONFLICT);
 				}
 			}
-			/*
-			 * if (statesRequestObj.getTransactionType().equalsIgnoreCase(DELETE)) {
-			 * statesResponse = new StatesResponse(); List<Integer> list= new
-			 * ArrayList<Integer>(); int count = statesDao.getAllStatesCount(); for(States
-			 * state:statesRequestObj.getStates()) { Integer id =state.getId();
-			 * list.add(id); boolean deleteState = statesDao.deleteStates(statesRequestObj);
-			 * logger.debug("inside  delete condition.****** and id is : " + list); if
-			 * (deleteState) {
-			 * statesResponse.setStatusMessage("Successfully record deleted");
-			 * statesResponse.setTotalCount(count); return new
-			 * ResponseEntity<>(statesResponse, HttpStatus.OK); } else {
-			 * statesResponse.setStatusMessage(FAILED); return new
-			 * ResponseEntity<>(statesResponse, HttpStatus.CONFLICT); } } }
-			 */
-			boolean b = (statesRequestObj.getTransactionType().equalsIgnoreCase(SAVE)
+			
+		boolean b = (statesRequestObj.getTransactionType().equalsIgnoreCase(SAVE)
 					|| statesRequestObj.getTransactionType().equalsIgnoreCase(UPDATE));
-					if (!(statesRequestObj.getTransactionType().equalsIgnoreCase(DELETE) || b)) {
+					if (! b) {
+						logger.debug("@@@@Inside setStates: transaction Type not correct");
 						statesResponse = new StatesResponse();
 						statesResponse.setStatusCode("422");
-						statesResponse.setStatusMessage("transaction type is not correct");
+						statesResponse.setMessage("transaction type is not correct");
 					}
 					return new ResponseEntity<>(statesResponse, HttpStatus.CONFLICT);
-		} catch (SQLException exception) {
-			logger.debug("inside designationService catch block.****");
-			ErrorResponse error = new ErrorResponse();
-			logger.debug("data is  invalid");
-			exception.printStackTrace();
-			error.setStatusMessage(exception.getMessage());
-			return new ResponseEntity<>(error, HttpStatus.CONFLICT);
-		}
+		 
 	}
     
 	@Override
 	public ResponseEntity<Object> getStates(StatesRequest statesRequestObj) throws SQLException {
 
 		StatesResponse statesResponse = new StatesResponse();
-		logger.debug("inside get in DesignationServiceImpl.***");
+		logger.info("@@@@Inside getStates in facade");
 		List<States> StateList=null;
 		if(statesRequestObj.getTransactionType().equalsIgnoreCase(GETALL)) {
 		 StateList = statesDao.getAll(statesRequestObj);
+		 statesResponse.setStatesList(StateList);
+		 statesResponse.setMessage("Record found");
+		 statesResponse.setStatusCode("200");
 		}else if(statesRequestObj.getTransactionType().equalsIgnoreCase(GETBYID)){
 			 StateList = statesDao.getStateById(statesRequestObj);
+			 statesResponse.setStatesList(StateList);
+			 statesResponse.setMessage("Record found");
+			 statesResponse.setStatusCode("200");
 		}
-		List<States> list = statesDao.getCountPerPage(StateList, statesRequestObj.getPageSize(),
-				statesRequestObj.getPageNo());
-		if (  list == null || StateList == null) { 
+		if ( StateList == null) { 
+			logger.debug("@@@@Inside getStates in facade::StateList is null");
 			statesResponse.setStatesList(new ArrayList<>());
-			statesResponse.setStatusMessage("No records found");
-			statesResponse.setTotalCount(0);
-		} else {
-
-			int count = statesDao.getAllStatesCount();
-			if (statesRequestObj.getPageNo() == 0 || statesRequestObj.getPageSize() == 0) {
-				statesResponse.setStatesList(StateList);
-				statesResponse.setStatusMessage("success");
-				statesResponse.setTotalCount(count);
-			} else {
-				statesResponse.setStatesList(list);
-				statesResponse.setStatusMessage("success");
-				statesResponse.setTotalCount(count);
-			}
+			statesResponse.setMessage("No records found");
+		
+		
 		}
 		return new ResponseEntity<Object>(statesResponse, HttpStatus.OK);
 	}

0d2900c921643a380496a3414d1c062ed63a2bd3
tshiva <tshiva@host.docker.internal>
2019-06-18 18:25:19 +0530

    removed pageNo and pageSize

--- zuul-auth-ojas-parent/ojas-obs-dependentdetails/src/main/java/com/ojas/obs/response/DependentDetailsResponse.java
+++ zuul-auth-ojas-parent/ojas-obs-dependentdetails/src/main/java/com/ojas/obs/response/DependentDetailsResponse.java
@@ -7,21 +7,8 @@ import com.ojas.obs.model.DependentDetails;
 public class DependentDetailsResponse {
 	private List<DependentDetails> getDependentDetailsList;
 	private String statusCode;
-	private String statusMessage;
-	private int pageNo;
-	private int pageSize;
-	private int totalCount;
+	private String message;
 	
-	public DependentDetailsResponse() {}
-	
-	public DependentDetailsResponse(List<DependentDetails> getDependentDetailsList,
-			int pageNo, int pageSize, int totalCount) {
-		super();
-		this.getDependentDetailsList = getDependentDetailsList;
-		this.pageNo = pageNo;
-		this.pageSize = pageSize;
-		this.totalCount = totalCount;
-	}
 	public List<DependentDetails> getGetDependentDetailsList() {
 		return getDependentDetailsList;
 	}
@@ -31,35 +18,57 @@ public class DependentDetailsResponse {
 	public String getStatusCode() {
 		return statusCode;
 	}
-
 	public void setStatusCode(String statusCode) {
 		this.statusCode = statusCode;
 	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	@Override
+	public String toString() {
+		return "DependentDetailsResponse [getDependentDetailsList=" + getDependentDetailsList + ", statusCode="
+				+ statusCode + ", message=" + message + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((getDependentDetailsList == null) ? 0 : getDependentDetailsList.hashCode());
+		result = prime * result + ((message == null) ? 0 : message.hashCode());
+		result = prime * result + ((statusCode == null) ? 0 : statusCode.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DependentDetailsResponse other = (DependentDetailsResponse) obj;
+		if (getDependentDetailsList == null) {
+			if (other.getDependentDetailsList != null)
+				return false;
+		} else if (!getDependentDetailsList.equals(other.getDependentDetailsList))
+			return false;
+		if (message == null) {
+			if (other.message != null)
+				return false;
+		} else if (!message.equals(other.message))
+			return false;
+		if (statusCode == null) {
+			if (other.statusCode != null)
+				return false;
+		} else if (!statusCode.equals(other.statusCode))
+			return false;
+		return true;
+	}
+	
+	
 
-	public String getStatusMessage() {
-		return statusMessage;
-	}
 
-	public void setStatusMessage(String statusMessage) {
-		this.statusMessage = statusMessage;
-	}
-
-	public int getPageNo() {
-		return pageNo;
-	}
-	public void setPageNo(int pageNo) {
-		this.pageNo = pageNo;
-	}
-	public int getPageSize() {
-		return pageSize;
-	}
-	public void setPageSize(int pageSize) {
-		this.pageSize = pageSize;
-	}
-	public int getTotalCount() {
-		return totalCount;
-	}
-	public void setTotalCount(int totalCount) {
-		this.totalCount = totalCount;
-	}
 }

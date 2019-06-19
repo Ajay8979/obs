edce4b0e5ea5e8bf1093e84d78b4cb02104db096
tshiva <tshiva@host.docker.internal>
2019-06-19 12:20:39 +0530

    added  statusMessage field

--- zuul-auth-ojas-parent/ojas-obs-employee-education-details/src/main/java/com/ojas/obs/emp_edu/model/EmployeeEducationResponse.java
+++ zuul-auth-ojas-parent/ojas-obs-employee-education-details/src/main/java/com/ojas/obs/emp_edu/model/EmployeeEducationResponse.java
@@ -4,44 +4,36 @@ import java.util.List;
 
 public class EmployeeEducationResponse {
 	private List<EmployeeEducationDetails> employeeEducationDetailsList;
-	private String statusMessage;
+	private String message;
 	private String statusCode;
-
 	public List<EmployeeEducationDetails> getEmployeeEducationDetailsList() {
 		return employeeEducationDetailsList;
 	}
-
 	public void setEmployeeEducationDetailsList(List<EmployeeEducationDetails> employeeEducationDetailsList) {
 		this.employeeEducationDetailsList = employeeEducationDetailsList;
 	}
-
-	public String getStatusMessage() {
-		return statusMessage;
+	public String getMessage() {
+		return message;
 	}
-
-	public void setStatusMessage(String statusMessage) {
-		this.statusMessage = statusMessage;
+	public void setMessage(String message) {
+		this.message = message;
 	}
-
 	public String getStatusCode() {
 		return statusCode;
 	}
-
 	public void setStatusCode(String statusCode) {
 		this.statusCode = statusCode;
 	}
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result
 				+ ((employeeEducationDetailsList == null) ? 0 : employeeEducationDetailsList.hashCode());
+		result = prime * result + ((message == null) ? 0 : message.hashCode());
 		result = prime * result + ((statusCode == null) ? 0 : statusCode.hashCode());
-		result = prime * result + ((statusMessage == null) ? 0 : statusMessage.hashCode());
 		return result;
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -56,17 +48,18 @@ public class EmployeeEducationResponse {
 				return false;
 		} else if (!employeeEducationDetailsList.equals(other.employeeEducationDetailsList))
 			return false;
+		if (message == null) {
+			if (other.message != null)
+				return false;
+		} else if (!message.equals(other.message))
+			return false;
 		if (statusCode == null) {
 			if (other.statusCode != null)
 				return false;
 		} else if (!statusCode.equals(other.statusCode))
 			return false;
-		if (statusMessage == null) {
-			if (other.statusMessage != null)
-				return false;
-		} else if (!statusMessage.equals(other.statusMessage))
-			return false;
 		return true;
 	}
 
+	
 }

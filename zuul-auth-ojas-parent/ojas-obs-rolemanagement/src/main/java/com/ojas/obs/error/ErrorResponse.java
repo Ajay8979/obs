c3c98343473e4b17d522f6bfd1a159f6cfea91fe
tshiva <tshiva@host.docker.internal>
2019-06-19 16:25:31 +0530

    added statusMessage field

--- zuul-auth-ojas-parent/ojas-obs-rolemanagement/src/main/java/com/ojas/obs/error/ErrorResponse.java
+++ zuul-auth-ojas-parent/ojas-obs-rolemanagement/src/main/java/com/ojas/obs/error/ErrorResponse.java
@@ -3,7 +3,14 @@ package com.ojas.obs.error;
 public class ErrorResponse {
 	private String message;
 	private String statusCode;
+
+	private String statusMessage;
+
 	
+
+	
+	
+
 	public String getMessage() {
 		return message;
 	}
@@ -16,6 +23,13 @@ public class ErrorResponse {
 	public void setStatusCode(String statusCode) {
 		this.statusCode = statusCode;
 	}
+	public String getStatusMessage() {
+		return statusMessage;
+	}
+	public void setStatusMessage(String statusMessage) {
+		this.statusMessage = statusMessage;
+	}
+	
 	
 	
 

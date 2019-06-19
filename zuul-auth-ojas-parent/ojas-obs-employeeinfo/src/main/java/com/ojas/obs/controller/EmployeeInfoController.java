f3a273c40aca8968f27eee85001336ddb18de098
tshiva <tshiva@host.docker.internal>
2019-06-19 12:36:13 +0530

    added duplicateEntryException catch block

--- zuul-auth-ojas-parent/ojas-obs-employeeinfo/src/main/java/com/ojas/obs/controller/EmployeeInfoController.java
+++ zuul-auth-ojas-parent/ojas-obs-employeeinfo/src/main/java/com/ojas/obs/controller/EmployeeInfoController.java
@@ -1,12 +1,14 @@
 package com.ojas.obs.controller;
 
-
 import static com.ojas.obs.constants.URLconstants.GET;
 import static com.ojas.obs.constants.URLconstants.SET;
+import static com.ojas.obs.constants.URLconstants.EMPLOYEEINFO;
 import static com.ojas.obs.constants.UserConstants.DELETE;
 import static com.ojas.obs.constants.UserConstants.SAVE;
 import static com.ojas.obs.constants.UserConstants.UPDATE;
-//import static com.ojas.obs.constants.URLconstants.EMPLOYEEINFO;
+import static com.ojas.obs.constants.UserConstants.EXCEPTION;
+import static com.ojas.obs.constants.UserConstants.SQLEXCEPTION;
+
 
 import java.sql.SQLException;
 import java.util.List;
@@ -16,6 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -54,75 +57,75 @@ public class EmployeeInfoController {
 
 	@RequestMapping(SET)
 	public ResponseEntity<Object> setEmployeeInfo(@RequestBody EmployeeInfoRequest employeeInfoRequest,
-			HttpServletRequest servletRequest, HttpServletResponse servletResponse) throws SQLException {
-
-		ResponseEntity<Object> responseEntity = null;
+			HttpServletRequest servletRequest, HttpServletResponse servletResponse) {
 
 		try {
 
 			if (null == employeeInfoRequest) {
-
 				ErrorResponse error = new ErrorResponse();
 				logger.debug("data is not valid");
+				error.setStatusCode("422");
 				error.setMessage("Request is not valid");
 				return new ResponseEntity<>(error, HttpStatus.UNPROCESSABLE_ENTITY);
 			}
-
 			List<EmployeeInfo> empInfolist = employeeInfoRequest.getEmployeeInfo();
-
 			if (null == empInfolist) {
 				ErrorResponse error = new ErrorResponse();
 				error.setMessage("data must not be null");
+				error.setStatusCode("422");
 				logger.debug("data is not valid");
 				return new ResponseEntity<>(error, HttpStatus.UNPROCESSABLE_ENTITY);
 			}
 			transactionType = employeeInfoRequest.getTransactionType();
-
 			for (EmployeeInfo empInfo : empInfolist) {
-
 				if (transactionType.equalsIgnoreCase(SAVE)
-
 						&& ((null == empInfo.getFirstname() || empInfo.getFirstname().isEmpty())
 								|| (null == empInfo.getMiddlename() || empInfo.getMiddlename().isEmpty())
 								|| (null == empInfo.getLastname() || empInfo.getLastname().isEmpty())
-								|| (null == empInfo.getStatus() || empInfo.getStatus().isEmpty())
-								|| (null == empInfo.getGender() || empInfo.getGender().isEmpty())
+								|| (null == empInfo.getStatus()) || (null == empInfo.getGender())
 								|| (null == empInfo.getDob() || empInfo.getDob().isEmpty())
-								|| (null == empInfo.getTitle())
-								|| null == empInfo.getEmployeeId() || empInfo.getEmployeeId().isEmpty()
-								|| (employeeInfoRequest.getTransactionType() == null
-										|| (employeeInfoRequest.getTransactionType().isEmpty())))) {
-
+								|| (null == empInfo.getTitle()) || null == empInfo.getEmployeeId()
+								|| empInfo.getEmployeeId().isEmpty())) {
 					ErrorResponse error = new ErrorResponse();
 					logger.debug("data is  invalid");
 					error.setMessage("Request is  invalid");
-
-					return new ResponseEntity<Object>(error, HttpStatus.UNPROCESSABLE_ENTITY);
-
+					error.setStatusCode("422");
+					return new ResponseEntity<>(error, HttpStatus.UNPROCESSABLE_ENTITY);
 				}
 
-				if ((transactionType.equalsIgnoreCase(UPDATE) && null == empInfo.getId()) || (transactionType.equalsIgnoreCase(DELETE) && null == empInfo.getId())) {
-
+				if ((transactionType.equalsIgnoreCase(UPDATE) && null == empInfo.getId())
+						|| (transactionType.equalsIgnoreCase(DELETE) && null == empInfo.getId())) {
 					ErrorResponse errorResponse = new ErrorResponse();
 					errorResponse = new ErrorResponse();
 					errorResponse.setStatusCode("422");
-					errorResponse.setMessage("id cannot be null");
-
+					errorResponse.setMessage("Id must not be null");
 					logger.info("Request is not valid, No id provided");
-					return new ResponseEntity<Object>(errorResponse, HttpStatus.UNPROCESSABLE_ENTITY);
+					return new ResponseEntity<>(errorResponse, HttpStatus.UNPROCESSABLE_ENTITY);
 				}
 			}
-			responseEntity = employeeInfoFacade.setEmployeeInfo(employeeInfoRequest);
-			return responseEntity;
-
-		} catch (Exception e) {
-
-			logger.debug("inside catch block.*******");
+			return employeeInfoFacade.setEmployeeInfo(employeeInfoRequest);
+		} catch (DuplicateKeyException e) {
+			logger.debug("DuplicateKeyException caught!");
 			ErrorResponse error = new ErrorResponse();
-			error.setMessage(e.getMessage());
+			error.setStatusCode("409");
+			error.setMessage("Duplicates are not allowed!");
+			error.setStatusMessage(e.getCause().getLocalizedMessage());
+			return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+		} catch (SQLException e) {
+			logger.debug("SQLException caught!");
+			ErrorResponse error = new ErrorResponse();
+			error.setStatusCode("409");
+			error.setMessage(SQLEXCEPTION);
+			error.setStatusMessage(e.getMessage());
+			return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+		} catch (Exception e) {
+			logger.debug("Exception caught!");
+			ErrorResponse error = new ErrorResponse();
+			error.setStatusCode("409");
+			error.setMessage(EXCEPTION);
+			error.setStatusMessage(e.getMessage());
+			return new ResponseEntity<>(error, HttpStatus.CONFLICT);
 		}
-		return responseEntity;
-
 	}
 
 	/**
@@ -131,39 +134,43 @@ public class EmployeeInfoController {
 	 * @param servletRequest
 	 * @param servletResponse
 	 * @return
-	 * @throws SQLException
 	 */
 
 	@RequestMapping(GET)
 	public ResponseEntity<Object> getEmpDetails(@RequestBody EmployeeInfoRequest employeeInfoRequest,
-			HttpServletRequest servletRequest, HttpServletResponse servletResponse) throws SQLException {
-        System.out.println("Inside controller");
-		ResponseEntity<Object> responseEntity = null;
+			HttpServletRequest servletRequest, HttpServletResponse servletResponse) {
 		try {
-
 			if (null == employeeInfoRequest) {
 
 				ErrorResponse error = new ErrorResponse();
 				logger.debug("data is not valid");
+				error.setStatusCode("422");
 				error.setMessage("Request is not valid");
 				return new ResponseEntity<>(error, HttpStatus.UNPROCESSABLE_ENTITY);
 			}
-
 			if (null == employeeInfoRequest.getTransactionType()
 					|| employeeInfoRequest.getTransactionType().isEmpty()) {
 				ErrorResponse error = new ErrorResponse();
 				logger.debug("data is not valid");
-				error.setMessage("transactiontype can't be null");
+				error.setStatusCode("422");
+				error.setMessage("Transaction type should not be null");
 				return new ResponseEntity<>(error, HttpStatus.UNPROCESSABLE_ENTITY);
 			}
 			return employeeInfoFacade.getAllEmployeeDetails(employeeInfoRequest);
-
-		} catch (Exception exception) {
-			logger.debug("inside catch block.*******");
-
+		} catch (SQLException exception) {
+			logger.debug("SQLException caught");
 			ErrorResponse error = new ErrorResponse();
+			error.setStatusCode("409");
+			error.setMessage(SQLEXCEPTION);
 			error.setStatusMessage(exception.getMessage());
+			return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+		} catch (Exception exception) {
+			logger.debug("Exception caught");
+			ErrorResponse error = new ErrorResponse();
+			error.setStatusCode("409");
+			error.setMessage(EXCEPTION);
+			error.setStatusMessage(exception.getMessage());
+			return new ResponseEntity<>(error, HttpStatus.CONFLICT);
 		}
-		return responseEntity;
 	}
 }

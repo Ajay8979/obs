a82953844f935904be5e4aae2ae6404f37a88c8c
tshiva <tshiva@host.docker.internal>
2019-06-19 16:40:00 +0530

    added statusMessages

--- zuul-auth-ojas-parent/ojas-obs-businessunit/src/main/java/com/obs/businessunit/error/ErrorResponse.java
+++ zuul-auth-ojas-parent/ojas-obs-businessunit/src/main/java/com/obs/businessunit/error/ErrorResponse.java
@@ -2,7 +2,15 @@ package com.obs.businessunit.error;
 
 public class ErrorResponse {
 	private String code;
+	private String statusMessage;
 	private String message;
+	
+	public String getStatusMessage() {
+		return statusMessage;
+	}
+	public void setStatusMessage(String statusMessage) {
+		this.statusMessage = statusMessage;
+	}
 	public String getCode() {
 		return code;
 	}

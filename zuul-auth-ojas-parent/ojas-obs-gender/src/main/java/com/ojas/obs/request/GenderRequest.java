315daf7abb297c894c1ae94d0b9a6ab2ec3944ff
tshiva <tshiva@host.docker.internal>
2019-06-19 13:03:39 +0530

    removed pageNoand pageSize

--- zuul-auth-ojas-parent/ojas-obs-gender/src/main/java/com/ojas/obs/request/GenderRequest.java
+++ zuul-auth-ojas-parent/ojas-obs-gender/src/main/java/com/ojas/obs/request/GenderRequest.java
@@ -6,34 +6,12 @@ import com.ojas.obs.model.Genders;
 
 public class GenderRequest {
   private List<Genders> gender;
-  private String sessionId;
-  private int pageNo;
-  private int pageSize;
   private String transactionType;
-
 public List<Genders> getGender() {
 	return gender;
 }
 public void setGender(List<Genders> gender) {
 	this.gender = gender;
-}
-public String getSessionId() {
-	return sessionId;
-}
-public void setSessionId(String sessionId) {
-	this.sessionId = sessionId;
-}
-public int getPageNo() {
-	return pageNo;
-}
-public void setPageNo(int pageNo) {
-	this.pageNo = pageNo;
-}
-public int getPageSize() {
-	return pageSize;
-}
-public void setPageSize(int pageSize) {
-	this.pageSize = pageSize;
 }
 public String getTransactionType() {
 	return transactionType;
@@ -41,6 +19,35 @@ public String getTransactionType() {
 public void setTransactionType(String transactionType) {
 	this.transactionType = transactionType;
 }
-  
+@Override
+public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((gender == null) ? 0 : gender.hashCode());
+	result = prime * result + ((transactionType == null) ? 0 : transactionType.hashCode());
+	return result;
+}
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	GenderRequest other = (GenderRequest) obj;
+	if (gender == null) {
+		if (other.gender != null)
+			return false;
+	} else if (!gender.equals(other.gender))
+		return false;
+	if (transactionType == null) {
+		if (other.transactionType != null)
+			return false;
+	} else if (!transactionType.equals(other.transactionType))
+		return false;
+	return true;
+}
+
 
 }

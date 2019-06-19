fe3991e56bcf280774d54ef19ce1a7fea724fe96
tshiva <tshiva@host.docker.internal>
2019-06-19 14:08:03 +0530

    removed pageNo and pageSize

--- zuul-auth-ojas-parent/ojas-obs-kye/src/main/java/com/ojas/obs/response/KyeResponse.java
+++ zuul-auth-ojas-parent/ojas-obs-kye/src/main/java/com/ojas/obs/response/KyeResponse.java
@@ -11,58 +11,66 @@ import com.ojas.obs.model.KYE;
  */
 public class KyeResponse {
 	private List<KYE> kyeList;
-	private int pageNo;
-	private int pageSize;
-	private int totalCount;
-	private String statusMessage;
+	private String message;
 	private String statusCode;
-
-	public String getStatusMessage() {
-		return statusMessage;
-	}
-
-	public void setStatusMessage(String statusMessage) {
-		this.statusMessage = statusMessage;
-	}
-
-	public String getStatusCode() {
-		return statusCode;
-	}
-
-	public void setStatusCode(String statusCode) {
-		this.statusCode = statusCode;
-	}
-
 	public List<KYE> getKyeList() {
 		return kyeList;
 	}
-
 	public void setKyeList(List<KYE> kyeList) {
 		this.kyeList = kyeList;
 	}
-
-	public int getPageNo() {
-		return pageNo;
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getStatusCode() {
+		return statusCode;
+	}
+	public void setStatusCode(String statusCode) {
+		this.statusCode = statusCode;
+	}
+	@Override
+	public String toString() {
+		return "KyeResponse [kyeList=" + kyeList + ", message=" + message + ", statusCode=" + statusCode + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((kyeList == null) ? 0 : kyeList.hashCode());
+		result = prime * result + ((message == null) ? 0 : message.hashCode());
+		result = prime * result + ((statusCode == null) ? 0 : statusCode.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		KyeResponse other = (KyeResponse) obj;
+		if (kyeList == null) {
+			if (other.kyeList != null)
+				return false;
+		} else if (!kyeList.equals(other.kyeList))
+			return false;
+		if (message == null) {
+			if (other.message != null)
+				return false;
+		} else if (!message.equals(other.message))
+			return false;
+		if (statusCode == null) {
+			if (other.statusCode != null)
+				return false;
+		} else if (!statusCode.equals(other.statusCode))
+			return false;
+		return true;
 	}
 
-	public void setPageNo(int pageNo) {
-		this.pageNo = pageNo;
-	}
-
-	public int getPageSize() {
-		return pageSize;
-	}
-
-	public void setPageSize(int pageSize) {
-		this.pageSize = pageSize;
-	}
-
-	public int getTotalCount() {
-		return totalCount;
-	}
-
-	public void setTotalCount(int totalCount) {
-		this.totalCount = totalCount;
-	}
+	
 
 }

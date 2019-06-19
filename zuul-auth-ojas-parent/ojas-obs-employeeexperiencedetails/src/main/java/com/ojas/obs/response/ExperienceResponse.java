24364d35acbcaaa4b76b464b58d5846b91f1d823
tshiva <tshiva@host.docker.internal>
2019-06-19 12:31:15 +0530

    removed constructor and added toString method

--- zuul-auth-ojas-parent/ojas-obs-employeeexperiencedetails/src/main/java/com/ojas/obs/response/ExperienceResponse.java
+++ zuul-auth-ojas-parent/ojas-obs-employeeexperiencedetails/src/main/java/com/ojas/obs/response/ExperienceResponse.java
@@ -7,51 +7,37 @@ import com.ojas.obs.model.EmployeeExperienceDetails;
 public class ExperienceResponse {
 	private List<EmployeeExperienceDetails> employeeExperienceDetails;
 	private String statusCode;
-	private String statusMessage;
-	public ExperienceResponse() {
-		super();
-	}
-	public ExperienceResponse(List<EmployeeExperienceDetails> employeeExperienceDetails,
-			String statusCode, String statusMessage) {
-		super();
-		this.employeeExperienceDetails = employeeExperienceDetails;
-		this.statusCode = statusCode;
-		this.statusMessage = statusMessage;
-	}
+	private String message;
+
 	public List<EmployeeExperienceDetails> getEmployeeExperienceDetails() {
 		return employeeExperienceDetails;
 	}
+
 	public void setEmployeeExperienceDetails(List<EmployeeExperienceDetails> employeeExperienceDetails) {
 		this.employeeExperienceDetails = employeeExperienceDetails;
 	}
+
 	public String getStatusCode() {
 		return statusCode;
 	}
+
 	public void setStatusCode(String statusCode) {
 		this.statusCode = statusCode;
 	}
-	public String getStatusMessage() {
-		return statusMessage;
+
+	public String getMessage() {
+		return message;
 	}
-	public void setStatusMessage(String statusMessage) {
-		this.statusMessage = statusMessage;
+
+	public void setMessage(String message) {
+		this.message = message;
 	}
-	/*
-	 * @Override public int hashCode() { final int prime = 31; int result = 1;
-	 * result = prime * result + ((employeeExperienceDetails == null) ? 0 :
-	 * employeeExperienceDetails.hashCode()); result = prime * result + ((statusCode
-	 * == null) ? 0 : statusCode.hashCode()); result = prime * result +
-	 * ((statusMessage == null) ? 0 : statusMessage.hashCode()); return result; }
-	 * 
-	 * @Override public boolean equals(Object obj) { if (this == obj) return true;
-	 * if (obj == null) return false; if (getClass() != obj.getClass()) return
-	 * false; ExperienceResponse other = (ExperienceResponse) obj; if
-	 * (employeeExperienceDetails == null) { if (other.employeeExperienceDetails !=
-	 * null) return false; } else if
-	 * (!employeeExperienceDetails.equals(other.employeeExperienceDetails)) return
-	 * false; if (statusCode == null) { if (other.statusCode != null) return false;
-	 * } else if (!statusCode.equals(other.statusCode)) return false; if
-	 * (statusMessage == null) { if (other.statusMessage != null) return false; }
-	 * else if (!statusMessage.equals(other.statusMessage)) return false; return
-	 * true; }
-	 */}
+
+	@Override
+	public String toString() {
+		return "ExperienceResponse [employeeExperienceDetails=" + employeeExperienceDetails + ", statusCode="
+				+ statusCode + ", message=" + message + "]";
+	}
+
+	
+}

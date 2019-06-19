6ca250e26f196d5ff99f28a5afb236d2391d012b
tshiva <tshiva@host.docker.internal>
2019-06-19 13:04:15 +0530

    added statusMessage field

--- zuul-auth-ojas-parent/ojas-obs-gender/src/main/java/com/ojas/obs/response/GenderResponse.java
+++ zuul-auth-ojas-parent/ojas-obs-gender/src/main/java/com/ojas/obs/response/GenderResponse.java
@@ -6,8 +6,7 @@ import com.ojas.obs.model.Genders;
 
 public class GenderResponse {
 	private List<Genders> genderList;
-	private String statusMessage;
-	private int totalCount;
+	private String message;
 	private String statusCode;
 	public List<Genders> getGenderList() {
 		return genderList;
@@ -15,8 +14,11 @@ public class GenderResponse {
 	public void setGenderList(List<Genders> genderList) {
 		this.genderList = genderList;
 	}
-	public String getStatusMessage() {
-		return statusMessage;
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
 	}
 	public String getStatusCode() {
 		return statusCode;
@@ -24,13 +26,40 @@ public class GenderResponse {
 	public void setStatusCode(String statusCode) {
 		this.statusCode = statusCode;
 	}
-	public void setStatusMessage(String statusMessage) {
-		this.statusMessage = statusMessage;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((genderList == null) ? 0 : genderList.hashCode());
+		result = prime * result + ((message == null) ? 0 : message.hashCode());
+		result = prime * result + ((statusCode == null) ? 0 : statusCode.hashCode());
+		return result;
 	}
-	public int getTotalCount() {
-		return totalCount;
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GenderResponse other = (GenderResponse) obj;
+		if (genderList == null) {
+			if (other.genderList != null)
+				return false;
+		} else if (!genderList.equals(other.genderList))
+			return false;
+		if (message == null) {
+			if (other.message != null)
+				return false;
+		} else if (!message.equals(other.message))
+			return false;
+		if (statusCode == null) {
+			if (other.statusCode != null)
+				return false;
+		} else if (!statusCode.equals(other.statusCode))
+			return false;
+		return true;
 	}
-	public void setTotalCount(int totalCount) {
-		this.totalCount = totalCount;
-	}
+	
 }

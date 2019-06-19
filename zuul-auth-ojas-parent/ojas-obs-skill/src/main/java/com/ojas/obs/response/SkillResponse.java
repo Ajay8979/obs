2ee6a15d6aafa71ee46cfe2ea04b540fd999f2f0
tshiva <tshiva@host.docker.internal>
2019-06-19 16:35:28 +0530

    removed statusCode

--- zuul-auth-ojas-parent/ojas-obs-skill/src/main/java/com/ojas/obs/response/SkillResponse.java
+++ zuul-auth-ojas-parent/ojas-obs-skill/src/main/java/com/ojas/obs/response/SkillResponse.java
@@ -7,25 +7,60 @@ import com.ojas.obs.model.Skill;
 public class SkillResponse {
 	
 	private List<Skill> listOfSkill;
-	private int totalCount;
-	private String statusMessage;
-	public int getTotalCount() {
-		return totalCount;
-	}
-	public void setTotalCount(int totalCount) {
-		this.totalCount = totalCount;
-	}
-	public String getStatusMessage() {
-		return statusMessage;
-	}
-	public void setStatusMessage(String statusMessage) {
-		this.statusMessage = statusMessage;
-	}
+   private String statusCode;
+	private String message;
 	public List<Skill> getListOfSkill() {
 		return listOfSkill;
 	}
 	public void setListOfSkill(List<Skill> listOfSkill) {
 		this.listOfSkill = listOfSkill;
 	}
-
+	public String getStatusCode() {
+		return statusCode;
+	}
+	public void setStatusCode(String statusCode) {
+		this.statusCode = statusCode;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((listOfSkill == null) ? 0 : listOfSkill.hashCode());
+		result = prime * result + ((message == null) ? 0 : message.hashCode());
+		result = prime * result + ((statusCode == null) ? 0 : statusCode.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SkillResponse other = (SkillResponse) obj;
+		if (listOfSkill == null) {
+			if (other.listOfSkill != null)
+				return false;
+		} else if (!listOfSkill.equals(other.listOfSkill))
+			return false;
+		if (message == null) {
+			if (other.message != null)
+				return false;
+		} else if (!message.equals(other.message))
+			return false;
+		if (statusCode == null) {
+			if (other.statusCode != null)
+				return false;
+		} else if (!statusCode.equals(other.statusCode))
+			return false;
+		return true;
+	}
+	
 }

3d7cd4aca6c49337ff45a305abd689d00313fa90
tshiva <tshiva@host.docker.internal>
2019-06-19 16:27:37 +0530

    removed statusMessage field

--- zuul-auth-ojas-parent/ojas-obs-rolemanagement/src/main/java/com/ojas/obs/response/RoleManagementResponse.java
+++ zuul-auth-ojas-parent/ojas-obs-rolemanagement/src/main/java/com/ojas/obs/response/RoleManagementResponse.java
@@ -12,39 +12,36 @@ import com.ojas.obs.model.RoleManagement;
 public class RoleManagementResponse {
 
 	private List<RoleManagement> roleManagementList;
-	private String statusMessage;
 
+	private String Message;
+	private String statusCode;
 	public List<RoleManagement> getRoleManagementList() {
 		return roleManagementList;
 	}
-
 	public void setRoleManagementList(List<RoleManagement> roleManagementList) {
 		this.roleManagementList = roleManagementList;
 	}
-
-	public String getStatusMessage() {
-		return statusMessage;
+	public String getMessage() {
+		return Message;
 	}
-
-	public void setStatusMessage(String statusMessage) {
-		this.statusMessage = statusMessage;
+	public void setMessage(String message) {
+		Message = message;
 	}
-
-	@Override
-	public String toString() {
-		return "RoleManagementResponse [roleManagementList=" + roleManagementList + ", statusMessage=" + statusMessage
-				+ "]";
+	public String getStatusCode() {
+		return statusCode;
 	}
-
+	public void setStatusCode(String statusCode) {
+		this.statusCode = statusCode;
+	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((Message == null) ? 0 : Message.hashCode());
 		result = prime * result + ((roleManagementList == null) ? 0 : roleManagementList.hashCode());
-		result = prime * result + ((statusMessage == null) ? 0 : statusMessage.hashCode());
+		result = prime * result + ((statusCode == null) ? 0 : statusCode.hashCode());
 		return result;
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -54,17 +51,24 @@ public class RoleManagementResponse {
 		if (getClass() != obj.getClass())
 			return false;
 		RoleManagementResponse other = (RoleManagementResponse) obj;
+		if (Message == null) {
+			if (other.Message != null)
+				return false;
+		} else if (!Message.equals(other.Message))
+			return false;
 		if (roleManagementList == null) {
 			if (other.roleManagementList != null)
 				return false;
 		} else if (!roleManagementList.equals(other.roleManagementList))
 			return false;
-		if (statusMessage == null) {
-			if (other.statusMessage != null)
+		if (statusCode == null) {
+			if (other.statusCode != null)
 				return false;
-		} else if (!statusMessage.equals(other.statusMessage))
+		} else if (!statusCode.equals(other.statusCode))
 			return false;
 		return true;
 	}
+
+	
 
 }

e5696863ab71aa654654a3e53c2691fd10a5000a
tshiva <tshiva@host.docker.internal>
2019-06-19 17:13:19 +0530

    removed unnecessary comments

--- zuul-auth-ojas-parent/ojas-obs-online-employeestatus-service/src/main/java/com/ojas/obs/model/EmployeeStatus.java
+++ zuul-auth-ojas-parent/ojas-obs-online-employeestatus-service/src/main/java/com/ojas/obs/model/EmployeeStatus.java
@@ -9,7 +9,7 @@ public class EmployeeStatus {
 
 	private Integer id;
 	private String status;
-	//private boolean delete;
+	// private boolean delete;
 
 	public Integer getId() {
 		return id;
@@ -41,8 +41,7 @@ public class EmployeeStatus {
 	@Override
 	public int hashCode() {
 		final int prime = 31;
-		int result = 1;
-//		result = prime * result + (delete ? 1231 : 1237);
+		int result = 1; //
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((status == null) ? 0 : status.hashCode());
 		return result;
@@ -57,9 +56,7 @@ public class EmployeeStatus {
 		if (getClass() != obj.getClass())
 			return false;
 		EmployeeStatus other = (EmployeeStatus) obj;
-		/*
-		 * if (delete != other.delete) return false;
-		 */		if (id == null) {
+		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))

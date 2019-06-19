e475e0cef51eb70addcacda6a6c4397a730e18f7
tshiva <tshiva@host.docker.internal>
2019-06-19 14:37:22 +0530

    removed pageNo and pageSize

--- zuul-auth-ojas-parent/ojas-obs-online-gpa/src/main/java/com/ojas/obs/request/GpaRequest.java
+++ zuul-auth-ojas-parent/ojas-obs-online-gpa/src/main/java/com/ojas/obs/request/GpaRequest.java
@@ -12,47 +12,13 @@ import com.ojas.obs.model.GpaPlan;
 public class GpaRequest {
 
 	private List<GpaPlan> gpaPlan;
-	private String sessionId;
-	private Integer totalCount;
-	private Integer pageSize;
-	private Integer pageNum;
-	private String statusMessage;
 	private String transactionType;
+	
 	public List<GpaPlan> getGpaPlan() {
 		return gpaPlan;
 	}
 	public void setGpaPlan(List<GpaPlan> gpaPlan) {
 		this.gpaPlan = gpaPlan;
-	}
-	public String getSessionId() {
-		return sessionId;
-	}
-	public void setSessionId(String sessionId) {
-		this.sessionId = sessionId;
-	}
-	public Integer getTotalCount() {
-		return totalCount;
-	}
-	public void setTotalCount(Integer totalCount) {
-		this.totalCount = totalCount;
-	}
-	public Integer getPageSize() {
-		return pageSize;
-	}
-	public void setPageSize(Integer pageSize) {
-		this.pageSize = pageSize;
-	}
-	public Integer getPageNum() {
-		return pageNum;
-	}
-	public void setPageNum(Integer pageNum) {
-		this.pageNum = pageNum;
-	}
-	public String getStatusMessage() {
-		return statusMessage;
-	}
-	public void setStatusMessage(String statusMessage) {
-		this.statusMessage = statusMessage;
 	}
 	public String getTransactionType() {
 		return transactionType;
@@ -65,11 +31,6 @@ public class GpaRequest {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((gpaPlan == null) ? 0 : gpaPlan.hashCode());
-		result = prime * result + ((pageNum == null) ? 0 : pageNum.hashCode());
-		result = prime * result + ((pageSize == null) ? 0 : pageSize.hashCode());
-		result = prime * result + ((sessionId == null) ? 0 : sessionId.hashCode());
-		result = prime * result + ((statusMessage == null) ? 0 : statusMessage.hashCode());
-		result = prime * result + ((totalCount == null) ? 0 : totalCount.hashCode());
 		result = prime * result + ((transactionType == null) ? 0 : transactionType.hashCode());
 		return result;
 	}
@@ -87,31 +48,6 @@ public class GpaRequest {
 				return false;
 		} else if (!gpaPlan.equals(other.gpaPlan))
 			return false;
-		if (pageNum == null) {
-			if (other.pageNum != null)
-				return false;
-		} else if (!pageNum.equals(other.pageNum))
-			return false;
-		if (pageSize == null) {
-			if (other.pageSize != null)
-				return false;
-		} else if (!pageSize.equals(other.pageSize))
-			return false;
-		if (sessionId == null) {
-			if (other.sessionId != null)
-				return false;
-		} else if (!sessionId.equals(other.sessionId))
-			return false;
-		if (statusMessage == null) {
-			if (other.statusMessage != null)
-				return false;
-		} else if (!statusMessage.equals(other.statusMessage))
-			return false;
-		if (totalCount == null) {
-			if (other.totalCount != null)
-				return false;
-		} else if (!totalCount.equals(other.totalCount))
-			return false;
 		if (transactionType == null) {
 			if (other.transactionType != null)
 				return false;
@@ -121,11 +57,8 @@ public class GpaRequest {
 	}
 	@Override
 	public String toString() {
-		return "GpaRequest [gpaPlan=" + gpaPlan + ", sessionId=" + sessionId + ", totalCount=" + totalCount
-				+ ", pageSize=" + pageSize + ", pageNum=" + pageNum + ", statusMessage=" + statusMessage
-				+ ", transactionType=" + transactionType + "]";
+		return "GpaRequest [gpaPlan=" + gpaPlan + ", transactionType=" + transactionType + "]";
 	}
-
 	
 }
 

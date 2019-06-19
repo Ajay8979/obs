f6f1ca6da38feb6845b7cb27c8d0fb832fed2d7f
tshiva <tshiva@host.docker.internal>
2019-06-19 18:49:31 +0530

    added toString method

--- zuul-auth-ojas-parent/ojas-obs-employeecontactinfo/src/main/java/com/ojas/obs/requests/EmployeeContactInfoRequest.java
+++ zuul-auth-ojas-parent/ojas-obs-employeecontactinfo/src/main/java/com/ojas/obs/requests/EmployeeContactInfoRequest.java
@@ -1,49 +1,33 @@
 package com.ojas.obs.requests;
 
-
-
-
-
 import java.util.List;
 
 import com.ojas.obs.model.EmployeeContactInfo;
 
-public class EmployeeContactInfoRequest { 
- 
-	 
-    private List<EmployeeContactInfo> empInfo;
-	
-	private String sessionId;
+public class EmployeeContactInfoRequest {
+
+	private List<EmployeeContactInfo> empInfo;
 	private String transactionType;
-	
-	
-	
-	
-	
-	
-
-
 
 	public List<EmployeeContactInfo> getEmpInfo() {
 		return empInfo;
 	}
+
 	public void setEmpInfo(List<EmployeeContactInfo> empInfo) {
 		this.empInfo = empInfo;
 	}
-	public String getSessionId() {
-		return sessionId;
-	}
-	public void setSessionId(String sessionId) {
-		this.sessionId = sessionId;
-	}
+
 	public String getTransactionType() {
 		return transactionType;
 	}
+
 	public void setTransactionType(String transactionType) {
 		this.transactionType = transactionType;
 	}
-	
-	
-	
-	
+
+	@Override
+	public String toString() {
+		return "EmployeeContactInfoRequest [empInfo=" + empInfo + ", transactionType=" + transactionType + "]";
+	}
+
 }

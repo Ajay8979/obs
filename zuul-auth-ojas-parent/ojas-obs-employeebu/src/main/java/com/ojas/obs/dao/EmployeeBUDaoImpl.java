5fc97c3aa47482c97b74ebfdee68f8b089df71f1
tshiva <tshiva@host.docker.internal>
2019-06-19 12:12:19 +0530

    removed try catch block

--- zuul-auth-ojas-parent/ojas-obs-employeebu/src/main/java/com/ojas/obs/dao/EmployeeBUDaoImpl.java
+++ zuul-auth-ojas-parent/ojas-obs-employeebu/src/main/java/com/ojas/obs/dao/EmployeeBUDaoImpl.java
@@ -42,7 +42,6 @@ public class EmployeeBUDaoImpl implements EmployeeBUDao {
 	 */
 	@Override
 	public boolean saveEmployeebu(EmployeeBUDetailsRequest employeebuRequest) throws SQLException {
-		try {
 		Timestamp timestamp = new Timestamp(new Date().getTime());
 		boolean flag = true;
 		int count = 0;
@@ -57,10 +56,7 @@ public class EmployeeBUDaoImpl implements EmployeeBUDao {
 			return true;
 		else
 			return status;
-	} finally {
-		jdbcTemplate.getDataSource().getConnection().close();
-	}
-	}
+	} 
 
 	/*
 	 * (non-Javadoc)
@@ -70,11 +66,8 @@ public class EmployeeBUDaoImpl implements EmployeeBUDao {
 	 */
 	@Override
 	public List<EmployeeBUDetails> getAllEmployeebu(EmployeeBUDetailsRequest employeebuRequest) throws SQLException {
-		try {
+		
 		return jdbcTemplate.query(GETEMPLOYEBU, new BeanPropertyRowMapper<EmployeeBUDetails>(EmployeeBUDetails.class));
-	} finally {
-		jdbcTemplate.getDataSource().getConnection().close();
-	}
 	}
 
 	/*
@@ -85,7 +78,7 @@ public class EmployeeBUDaoImpl implements EmployeeBUDao {
 	 */
 	@Override
 	public boolean updateEmployeebu(EmployeeBUDetailsRequest employeebuRequest) throws SQLException {
-		try {
+		
 		int count = 0;
 		Timestamp timestamp = new Timestamp(new Date().getTime());
 		List<EmployeeBUDetails> budetails = employeebuRequest.getEmployeeBUDeatils();
@@ -98,9 +91,7 @@ public class EmployeeBUDaoImpl implements EmployeeBUDao {
 			return true;
 		else
 			return false;
-	} finally {
-		jdbcTemplate.getDataSource().getConnection().close();
-	}
+	
 	}
 
 	/*
@@ -110,7 +101,7 @@ public class EmployeeBUDaoImpl implements EmployeeBUDao {
 	 */
 	@Override
 	public boolean deleteEmployeeRecord(Integer courseId) throws SQLException {
-		try {
+		
 		int count = 0;
 		boolean status = false;
 		count = jdbcTemplate.update(DELETEBUDEAILS, courseId);
@@ -118,9 +109,7 @@ public class EmployeeBUDaoImpl implements EmployeeBUDao {
 			return true;
 		else
 			return status;
-	} finally {
-		jdbcTemplate.getDataSource().getConnection().close();
-	}
+	
 	}
 
 	/*
@@ -130,16 +119,14 @@ public class EmployeeBUDaoImpl implements EmployeeBUDao {
 	 */
 	@Override
 	public int getAllRecordsCount() throws SQLException {
-		try {
+		
 		return jdbcTemplate.queryForObject(GETBUDetailsCOUNT, Integer.class);
-	} finally {
-		jdbcTemplate.getDataSource().getConnection().close();
-	}
+	
 	}
 
 	@Override
 	public List<EmployeeBUDetails> getById(EmployeeBUDetailsRequest employeebuRequest) throws SQLException {
-		try {
+		
 		List<EmployeeBUDetails> modelList = employeebuRequest.getEmployeeBUDeatils();
 		List<Object[]> list = new ArrayList<Object[]>();
 		Object[] param = null;
@@ -150,13 +137,11 @@ public class EmployeeBUDaoImpl implements EmployeeBUDao {
 		}
 		List<EmployeeBUDetails> query = jdbcTemplate.query(GETBYID, param, new BeanPropertyRowMapper<>(EmployeeBUDetails.class));
 		return query;
-	}finally {
-		jdbcTemplate.getDataSource().getConnection().close();
-	}
+	
 	}
 	@Override
 	public List<EmployeeBUDetails> getByEmpId(EmployeeBUDetailsRequest employeebuRequest) throws SQLException {
-		try {
+	
 		List<EmployeeBUDetails> modelList = employeebuRequest.getEmployeeBUDeatils();
 		List<Object[]> list = new ArrayList<Object[]>();
 		Object[] param = null;
@@ -167,8 +152,6 @@ public class EmployeeBUDaoImpl implements EmployeeBUDao {
 		}
 		List<EmployeeBUDetails> query = jdbcTemplate.query(GETEMP, param, new BeanPropertyRowMapper<>(EmployeeBUDetails.class));
 		return query;
-	}finally {
-		jdbcTemplate.getDataSource().getConnection().close();
-	}
+	
 	}
 }

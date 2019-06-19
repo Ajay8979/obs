4702ef7b4ae8bdb104b47e306c3de9ec4aea6896
tshiva <tshiva@host.docker.internal>
2019-06-19 14:08:51 +0530

    added statusMessage field

--- zuul-auth-ojas-parent/ojas-obs-kye/src/main/java/com/ojas/obs/utility/ErrorResponse.java
+++ zuul-auth-ojas-parent/ojas-obs-kye/src/main/java/com/ojas/obs/utility/ErrorResponse.java
@@ -2,23 +2,68 @@ package com.ojas.obs.utility;
 
 public class ErrorResponse {
 	
-	private String errorMessage;
-	private String errorCode;
-
-	public String getErrorMessage() {
-		return errorMessage;
+	private String statusMessage;
+	private String message;
+	private String statusCode;
+	public String getStatusMessage() {
+		return statusMessage;
+	}
+	public void setStatusMessage(String statusMessage) {
+		this.statusMessage = statusMessage;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getStatusCode() {
+		return statusCode;
+	}
+	public void setStatusCode(String statusCode) {
+		this.statusCode = statusCode;
+	}
+	@Override
+	public String toString() {
+		return "ErrorResponse [statusMessage=" + statusMessage + ", message=" + message + ", statusCode=" + statusCode
+				+ "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((message == null) ? 0 : message.hashCode());
+		result = prime * result + ((statusCode == null) ? 0 : statusCode.hashCode());
+		result = prime * result + ((statusMessage == null) ? 0 : statusMessage.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ErrorResponse other = (ErrorResponse) obj;
+		if (message == null) {
+			if (other.message != null)
+				return false;
+		} else if (!message.equals(other.message))
+			return false;
+		if (statusCode == null) {
+			if (other.statusCode != null)
+				return false;
+		} else if (!statusCode.equals(other.statusCode))
+			return false;
+		if (statusMessage == null) {
+			if (other.statusMessage != null)
+				return false;
+		} else if (!statusMessage.equals(other.statusMessage))
+			return false;
+		return true;
 	}
 
-	public void setErrorMessage(String errorMessage) {
-		this.errorMessage = errorMessage;
-	}
-
-	public String getErrorCode() {
-		return errorCode;
-	}
-
-	public void setErrorCode(String errorCode) {
-		this.errorCode = errorCode;
-	}
+	
 
 }

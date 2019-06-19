86b0d9099f09949614d6130f2994f476d7d08873
tshiva <tshiva@host.docker.internal>
2019-06-19 16:34:34 +0530

    added hashcode and equals methods

--- zuul-auth-ojas-parent/ojas-obs-skill/src/main/java/com/ojas/obs/request/SkillRequest.java
+++ zuul-auth-ojas-parent/ojas-obs-skill/src/main/java/com/ojas/obs/request/SkillRequest.java
@@ -21,6 +21,35 @@ public class SkillRequest {
 	public void setTransactionType(String transactionType) {
 		this.transactionType = transactionType;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((listOfSkill == null) ? 0 : listOfSkill.hashCode());
+		result = prime * result + ((transactionType == null) ? 0 : transactionType.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SkillRequest other = (SkillRequest) obj;
+		if (listOfSkill == null) {
+			if (other.listOfSkill != null)
+				return false;
+		} else if (!listOfSkill.equals(other.listOfSkill))
+			return false;
+		if (transactionType == null) {
+			if (other.transactionType != null)
+				return false;
+		} else if (!transactionType.equals(other.transactionType))
+			return false;
+		return true;
+	}
 	
 	
 }

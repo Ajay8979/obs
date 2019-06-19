d24e6a630b9b984c43e994903b8a8c27ac146398
tshiva <tshiva@host.docker.internal>
2019-06-19 16:01:24 +0530

    added proper logger messsages

--- zuul-auth-ojas-parent/ojas-obs-online-separationtype-service/src/main/java/com/ojas/obs/controller/SeparationTypeController.java
+++ zuul-auth-ojas-parent/ojas-obs-online-separationtype-service/src/main/java/com/ojas/obs/controller/SeparationTypeController.java
@@ -1,10 +1,7 @@
 package com.ojas.obs.controller;
 
-//import static com.ojas.obs.constants.SeparationTypeConstants.DELETE;
 import static com.ojas.obs.constants.SeparationTypeConstants.GET;
-//import static com.ojas.obs.constants.SeparationTypeConstants.SEPARATIONTYPE;
 import static com.ojas.obs.constants.SeparationTypeConstants.SET;
-import static com.ojas.obs.constants.SeparationTypeConstants.UPDATE;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -12,8 +9,9 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.jboss.logging.Logger;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -33,136 +31,122 @@ import com.ojas.obs.utility.ErrorResponse;
 
 /**
  * 
- * @author nsrikanth  
+ * @author nsrikanth
  *
  */
 @RestController
-//@RequestMapping(SEPARATIONTYPE) 
-public class SeparationTypeController {   
- 
+//@RequestMapping("/separationType")
+public class SeparationTypeController {
+
 	Logger logger = Logger.getLogger(this.getClass());
 
 	@Autowired
 	private SeparationTypeFacade separationTypeFacade;
 
-	/**
-	 *  
-	 * @param separationTypeRequest
-	 * @param httpServletRequest
-	 * @param httpServletResponse
-	 * @return
-	 * @throws SQLException
-	 */
 	@RequestMapping(value = SET, method = RequestMethod.POST)
 	public ResponseEntity<Object> setSeparationType(@RequestBody SeparationTypeRequest separationTypeRequest,
-			HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws SQLException {
-		ErrorResponse errorResponse = null; 
-		String transactionType = null; 
+			HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+
+		ErrorResponse errorResponse = null;
+		String transactionType = null;
+
+		logger.info("The incoming request inside setSeparationType controller method "
+				+ separationTypeRequest);
+		// String transactionType = null;
+
 		List<SeparationType> listSeparationType = separationTypeRequest.getSeparationType();
- 
-		
+
 		try {
 			if (null == listSeparationType || listSeparationType.isEmpty()) {
 				errorResponse = new ErrorResponse();
-				errorResponse.setStatusMessage("separationtypeRequest obj is not valid");
+				logger.error("ListSeparationType is null" + listSeparationType);
+				errorResponse.setMessage("separationtypeRequest obj is not valid");
 				errorResponse.setStatusCode("422");
 				/*
 				 * errorResponse.setStatusCode("422");
 				 * errorResponse.setMessage(SeparationTypeConstants.INVALID_REQUEST);
 				 */
-				logger.info("Request is not valid"); 
 				return new ResponseEntity<Object>(errorResponse, HttpStatus.UNPROCESSABLE_ENTITY);
 			}
-			for (SeparationType sep : listSeparationType) { 
+			for (SeparationType sep : listSeparationType) {
 
 				if ((separationTypeRequest.getTransactionType().equalsIgnoreCase("update")
-						|| separationTypeRequest.getTransactionType().equalsIgnoreCase("delete")) && null == sep.getSeparationTypeId()) {
+						|| separationTypeRequest.getTransactionType().equalsIgnoreCase("delete"))
+						&& null == sep.getSeparationTypeId()) {
 					ErrorResponse error = new ErrorResponse();
-					logger.debug("data is  invalid");
-					error.setStatusMessage("id can't be null");
+					logger.error("id is null in SeparationTypeController " + separationTypeRequest );
+					error.setMessage("id can't be null");
 					error.setStatusCode("422");
 					return new ResponseEntity<>(error, HttpStatus.UNPROCESSABLE_ENTITY);
 				}
 			}
 			return separationTypeFacade.setSeparationTypeDetails(separationTypeRequest);
 
-		} catch (Exception exception) {
-			logger.debug("inside catch block.*******");
+		} catch (DuplicateKeyException exception) {
+			
+			logger.error("Inside DuplicateKeyException catch block.");
 			ErrorResponse error = new ErrorResponse();
 			error.setStatusMessage(exception.getMessage());
+			error.setMessage("duplicates are not allowed");
 			error.setStatusCode("409");
+			//exception.printStackTrace();
+			return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+		} catch (SQLException exception) {
+			logger.error("Inside SQLException catch SQLblock.");
+			ErrorResponse error = new ErrorResponse();
+			error.setStatusMessage(exception.getMessage());
+			error.setMessage("SQLException");
+			error.setStatusCode("409");
+			//exception.printStackTrace();
+			return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+		} catch (Exception exception) {
+			logger.error("Inside Exception catch Exception block.");
+			ErrorResponse error = new ErrorResponse();
+			error.setStatusMessage(exception.getMessage());
+			error.setMessage("Exception");
+			error.setStatusCode("422");
+			//exception.printStackTrace();
 			return new ResponseEntity<>(error, HttpStatus.CONFLICT);
 		}
 
 	}
 
-		
-		
-		
-		
-	/*
-	 * for (SeparationType sep : listSeparationType) { if (null ==
-	 * separationTypeRequest.getSeparationType() ||
-	 * separationTypeRequest.getSeparationType().isEmpty()) { errorResponse = new
-	 * ErrorResponse(); errorResponse.setStatusMessage("422");
-	 * errorResponse.setStatusMessage(SeparationTypeConstants.INVALID_ENTITY);
-	 * logger.info("Request is not valid, Invalid entity"); return new
-	 * ResponseEntity<Object>(errorResponse, HttpStatus.UNPROCESSABLE_ENTITY); } if
-	 * (null == separationTypeRequest.getTransactionType()) { errorResponse = new
-	 * ErrorResponse(); errorResponse.setStatusCode("422");
-	 * errorResponse.setMessage(SeparationTypeConstants.INVALID_TRANSACTION_TYPE);
-	 * logger.info("Request is not valid, Invalid transaction type"); return new
-	 * ResponseEntity<Object>(errorResponse, HttpStatus.UNPROCESSABLE_ENTITY); } if
-	 * (null == separationTypeRequest.getSessionId()) { errorResponse = new
-	 * ErrorResponse(); errorResponse.setStatusCode("422");
-	 * errorResponse.setMessage(SeparationTypeConstants.INVALID_SESSION_ID);
-	 * logger.info("Request is not valid, Invalid session id"); return new
-	 * ResponseEntity<Object>(errorResponse, HttpStatus.UNPROCESSABLE_ENTITY); }
-	 * transactionType = separationTypeRequest.getTransactionType(); if
-	 * ((transactionType.equalsIgnoreCase(UPDATE) ||
-	 * transactionType.equalsIgnoreCase(DELETE)) && null ==
-	 * sep.getSeparationTypeId()) {
-	 * 
-	 * errorResponse = new ErrorResponse(); errorResponse.setStatusCode("422");
-	 * errorResponse.setMessage(SeparationTypeConstants.INVALID_ID);
-	 * logger.info("Request is not valid, No id provided"); return new
-	 * ResponseEntity<Object>(errorResponse, HttpStatus.UNPROCESSABLE_ENTITY);
-	 * 
-	 * } } return
-	 * separationTypeFacade.setSeparationTypeDetails(separationTypeRequest); }
-	 */
-
-	/**
-	 * 
-	 * @param separationTypeRequest
-	 * @param httpServletRequest
-	 * @param httpServletResponse
-	 * @return
-	 * @throws SQLException
-	 */
 	@RequestMapping(value = GET, method = RequestMethod.POST)
 	public ResponseEntity<Object> getAllSeparationType(@RequestBody SeparationTypeRequest separationTypeRequest,
-			HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws SQLException {
-		logger.info("Inside getAllSeparationType()");
-		ErrorResponse errorResponse = null;  
-		// Gson gson = new Gson();
-		// SeparationTypeRequest separationTypeReq =
-		// gson.fromJson(separationTypeRequest, SeparationTypeRequest.class);
-		if (null == separationTypeRequest) {
-			errorResponse = new ErrorResponse();
-			errorResponse.setStatusCode("422");
-			errorResponse.setMessage(SeparationTypeConstants.INVALID_REQUEST);
-			logger.info("Request is not valid");
-			return new ResponseEntity<Object>(errorResponse, HttpStatus.UNPROCESSABLE_ENTITY); 
+			HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+
+		try {
+			logger.info("The request Inside getAllSeparationType controller method " + separationTypeRequest);
+
+			ErrorResponse errorResponse = null;
+			if (null == separationTypeRequest) {
+				errorResponse = new ErrorResponse();
+				errorResponse.setStatusCode("422");
+				errorResponse.setMessage(SeparationTypeConstants.INVALID_REQUEST);
+				logger.error("separationTypeRequest is null" + separationTypeRequest);
+				return new ResponseEntity<Object>(errorResponse, HttpStatus.UNPROCESSABLE_ENTITY);
+			}
+
+			return separationTypeFacade.getSeparationType(separationTypeRequest);
+
+		} catch (SQLException exception) {
+			logger.error("Inside SQLException catch Exception block.");
+			ErrorResponse error = new ErrorResponse();
+			error.setStatusMessage(exception.getMessage());
+			error.setMessage("SQLException");
+			error.setStatusCode("409");
+			//exception.printStackTrace();
+			return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+		} catch (Exception e) {
+			//e.printStackTrace();
+			logger.error("Inside Exception catch SQLblock.");
+			ErrorResponse errorResponse = new ErrorResponse();
+			errorResponse.setStatusMessage(e.getMessage());
+			errorResponse.setMessage("Exception");
+			errorResponse.setStatusCode("409");
+
+			return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
 		}
-		if (null == separationTypeRequest.getSessionId()) {
-			errorResponse = new ErrorResponse();
-			errorResponse.setStatusCode("422");
-			errorResponse.setMessage(SeparationTypeConstants.INVALID_SESSION_ID);
-			logger.info("Request is not valid, Invalid session id");
-			return new ResponseEntity<Object>(errorResponse, HttpStatus.UNPROCESSABLE_ENTITY);
-		}
-		return separationTypeFacade.getSeparationType(separationTypeRequest);
 	}
 
 }

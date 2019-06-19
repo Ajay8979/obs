3943b713706190f34244a7c17c8db917d0083a0b
tshiva <tshiva@host.docker.internal>
2019-06-19 16:42:43 +0530

    added message field

--- zuul-auth-ojas-parent/ojas-obs-businessunit/src/main/java/com/obs/businessunit/response/BusinessUnitResponse.java
+++ zuul-auth-ojas-parent/ojas-obs-businessunit/src/main/java/com/obs/businessunit/response/BusinessUnitResponse.java
@@ -6,53 +6,25 @@ import com.obs.businessunit.model.BusinessUnit;
 
 public class BusinessUnitResponse {
 	List<BusinessUnit> businessUnitList;
-	private String statusMessage;
-
+	private String message;
+	private String statusCode;
 	public List<BusinessUnit> getBusinessUnitList() {
 		return businessUnitList;
 	}
-
 	public void setBusinessUnitList(List<BusinessUnit> businessUnitList) {
 		this.businessUnitList = businessUnitList;
 	}
-
-	public String getStatusMessage() {
-		return statusMessage;
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getStatusCode() {
+		return statusCode;
+	}
+	public void setStatusCode(String statusCode) {
+		this.statusCode = statusCode;
 	}
 
-	public void setStatusMessage(String statusMessage) {
-		this.statusMessage = statusMessage;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((businessUnitList == null) ? 0 : businessUnitList.hashCode());
-		result = prime * result + ((statusMessage == null) ? 0 : statusMessage.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		BusinessUnitResponse other = (BusinessUnitResponse) obj;
-		if (businessUnitList == null) {
-			if (other.businessUnitList != null)
-				return false;
-		} else if (!businessUnitList.equals(other.businessUnitList))
-			return false;
-		if (statusMessage == null) {
-			if (other.statusMessage != null)
-				return false;
-		} else if (!statusMessage.equals(other.statusMessage))
-			return false;
-
-		return true;
-	}
 }

365a31ff96755108414a16e779a6b6d939762f9f
tshiva <tshiva@host.docker.internal>
2019-06-19 18:59:07 +0530

    commented unnecessary code

--- zuul-auth-ojas-parent/ojas-obs-subbusinessunit/src/main/java/com/ojas/obs/constants/SubBusinessUnitConstants.java
+++ zuul-auth-ojas-parent/ojas-obs-subbusinessunit/src/main/java/com/ojas/obs/constants/SubBusinessUnitConstants.java
@@ -14,7 +14,8 @@ public class SubBusinessUnitConstants {
 	
 	public static final String SAVEBUSINESSUNIT = "Insert into obs_subbusinessunit(name,businessUnitId,CostCenterId) VALUES (?,?,?)";
 	public static final String UPDATEBUSINESSUNITID = "update obs_subbusinessunit set name = ?, businessUnitId = ?, CostCenterId = ? where id = ? ";
-	public static final String GETALLSUBBUSINESSUNITS = "select sbu.*, bu.businessUnitName from obs_subbusinessunit sbu, obs_businessunit bu WHERE bu.id = sbu.businessUnitId";
+	//public static final String GETALLSUBBUSINESSUNITS = "select sbu.*, bu.businessUnitName from obs_subbusinessunit sbu, obs_businessunit bu WHERE bu.id = sbu.businessUnitId";
+	public static final String GETALLSUBBUSINESSUNITS = "select * from obs_subbusinessunit";
 	public static final String GETBYIDBUSINESSUNITS = "select * from obs_subbusinessunit where id = ?";
 	
 

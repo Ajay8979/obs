cc42ae1446a32176cbbecc2bb71817dfee496599
tshiva <tshiva@host.docker.internal>
2019-06-19 18:46:47 +0530

    added sqlException

--- zuul-auth-ojas-parent/ojas-obs-employeecontactinfo/src/main/java/com/ojas/obs/daoimpl/EmployeeStatusDaoImpl.java
+++ zuul-auth-ojas-parent/ojas-obs-employeecontactinfo/src/main/java/com/ojas/obs/daoimpl/EmployeeStatusDaoImpl.java
@@ -1,21 +1,20 @@
 package com.ojas.obs.daoimpl;
 
+import java.sql.SQLException;
 import java.sql.Timestamp;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ojas.obs.dao.EmployeeContactRowMapper;
 import com.ojas.obs.dao.EmployeeStatusDao;
 import com.ojas.obs.model.EmployeeContactInfo;
 import com.ojas.obs.requests.EmployeeContactInfoRequest;
-import com.ojas.obs.response.EmployeeContactInfoResponse;
 
 /**
  * 
@@ -29,12 +28,11 @@ public class EmployeeStatusDaoImpl implements EmployeeStatusDao {
 	Logger logger = Logger.getLogger(this.getClass());
 
 	public static final String INSERTEMPLOYEECONTACTINFOSTMT = "INSERT INTO obs_employeecontactinfo(email,personal_mobileNo,alternate_mobileNo,current_Address_line1,current_Address_line2, current_City,current_State,current_Pin,permanent_Address_Line_1,emp_Id,created_By,created_date,flag) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)";
-	public static final String SHOWCONTACTINFOSTMT = "SELECT * FROM obs_employeecontactinfo WHERE id=?";
-	public static final String SHOWCONTACTINFOSTMTEMPID = "SELECT * FROM obs_employeecontactinfo WHERE emp_Id=?";
-	public static final String SHOWCONTACTINFOSTMTID = "SELECT * FROM obs_employeecontactinfo WHERE id=?";
+	public static final String SHOWCONTACTINFOSTMT = "SELECT * FROM obs_employeecontactinfo WHERE id=? and flag = 1";
+	public static final String SHOWCONTACTINFOSTMTEMPID = "SELECT * FROM obs_employeecontactinfo WHERE emp_Id=? and flag = 1";
 	public static final String UPDATESTMT = "UPDATE obs_employeecontactinfo set email = ? ,personal_mobileNo = ?,alternate_mobileNo = ?,current_Address_line1 = ?,current_Address_line2 = ?, current_City = ?,current_State = ?,current_Pin = ?,permanent_Address_Line_1 = ?,updated_By = ?,updated_date = ? WHERE id = ? ";
 	public static final String DELETECONTACT = "UPDATE obs_employeecontactinfo set flag = ?,updated_date = ?,updated_By = ? where id = ?";
-	public static final String TOTALRECORDS = "select * from obs_employeecontactinfo where flag=?";
+	public static final String TOTALRECORDS = "select * from obs_employeecontactinfo where flag=1";
 
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
@@ -46,62 +44,29 @@ public class EmployeeStatusDaoImpl implements EmployeeStatusDao {
 	 * requests.EmployeeContactInfoRequest)
 	 */
 
+	@Transactional(rollbackFor = Exception.class)
 	@Override
-	public String saveEmployeeContactInfo(EmployeeContactInfoRequest empRequest) {
+	public boolean saveEmployeeContactInfo(EmployeeContactInfoRequest empRequest) throws SQLException {
 
+		int[] save;
 		logger.debug("@Saving employee contact info dao method");
 		Timestamp sqlDate = new Timestamp(new java.util.Date().getTime());
 
 		List<Object[]> inputList = new ArrayList<Object[]>();
 		for (EmployeeContactInfo emp : empRequest.getEmpInfo()) {
-			Object[] tmp = { emp.getEmail(), emp.getPersonal_mobileNo(), emp.getAlternate_mobileNo(),
-					emp.getCurrent_Address_Line1(), emp.getCurrent_Address_Line2(), emp.getCurrent_City(),
-					emp.getCurrent_State(), emp.getCurrent_Pin(), emp.getPermanent_Address_Line_1(), emp.getEmp_Id(),
-					emp.getCreated_By(), sqlDate, true };
+			Object[] tmp = { emp.getEmail(), emp.getPersonalMobileNo(), emp.getAlternateMobileNo(),
+					emp.getCurrentAddressLine1(), emp.getCurrentAddressLine2(), emp.getCurrentCity(),
+					emp.getCurrentState(), emp.getCurrentPin(), emp.getPermanentAddressLine1(), emp.getEmpId(),
+					emp.getCreatedBy(), sqlDate, true };
 			inputList.add(tmp);
 
 		}
-		try {
-			jdbcTemplate.batchUpdate(INSERTEMPLOYEECONTACTINFOSTMT, inputList);
-		} finally {
-			if (jdbcTemplate != null) {
-				try {
-					jdbcTemplate.getDataSource().getConnection().close();
-				} catch (Exception e) {
-
-					e.printStackTrace();
-				}
-
-			}
-
+		save = jdbcTemplate.batchUpdate(INSERTEMPLOYEECONTACTINFOSTMT, inputList);
+		if (save.length > 0) {
+			return true;
+		} else {
+			return false;
 		}
-		return "success";
-	}
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.ojas.obs.dao.EmployeeStatusDao#showEmployeeContactInfo(com.ojas.obs.
-	 * requests.EmployeeContactInfoRequest)
-	 */
-
-	@Override
-	public EmployeeContactInfo showEmployeeContactInfo(EmployeeContactInfoRequest empRequest) {
-		EmployeeContactInfo emp = null;
-		logger.debug("@getting employee contact info based on his id");
-		try {
-			emp = jdbcTemplate.queryForObject(SHOWCONTACTINFOSTMT,
-					new Object[] { empRequest.getEmpInfo().get(0).getId() }, new EmployeeContactRowMapper());
-		} finally {
-			if (jdbcTemplate != null) {
-				try {
-					jdbcTemplate.getDataSource().getConnection().close();
-				} catch (Exception e) {
-
-					e.printStackTrace();
-				}
-			}
-		}
-		return emp;
 
 	}
 
@@ -112,27 +77,30 @@ public class EmployeeStatusDaoImpl implements EmployeeStatusDao {
 	 * com.ojas.obs.dao.EmployeeStatusDao#updateEmployeeContactInfo(com.ojas.obs.
 	 * requests.EmployeeContactInfoRequest)
 	 */
+
+	@Transactional(rollbackFor = Exception.class)
 	@Override
-	public String updateEmployeeContactInfo(EmployeeContactInfoRequest empRequest) {
+	public boolean updateEmployeeContactInfo(EmployeeContactInfoRequest empRequest) {
 
-		logger.debug("@updating employee contact info");
+		int[] update;
+		logger.debug("@updating employee contact info" + empRequest);
 		Timestamp sqlUpdateDate = new Timestamp(new java.util.Date().getTime());
-		try {
-			List<Object[]> inputList = new ArrayList<Object[]>();
-			for (EmployeeContactInfo emp : empRequest.getEmpInfo()) {
-				Object[] tmp = { emp.getEmail(), emp.getPersonal_mobileNo(), emp.getAlternate_mobileNo(),
-						emp.getCurrent_Address_Line1(), emp.getCurrent_Address_Line2(), emp.getCurrent_City(),
-						emp.getCurrent_State(), emp.getCurrent_Pin(), emp.getPermanent_Address_Line_1(),
-						emp.getUpdated_By(), sqlUpdateDate, emp.getId() };
-				inputList.add(tmp);
+		List<Object[]> inputList = new ArrayList<Object[]>();
+		for (EmployeeContactInfo emp : empRequest.getEmpInfo()) {
+			Object[] tmp = { emp.getEmail(), emp.getPersonalMobileNo(), emp.getAlternateMobileNo(),
+					emp.getCurrentAddressLine1(), emp.getCurrentAddressLine2(), emp.getCurrentCity(),
+					emp.getCurrentState(), emp.getCurrentPin(), emp.getPermanentAddressLine1(), emp.getUpdatedBy(),
+					sqlUpdateDate, emp.getId() };
+			inputList.add(tmp);
 
-			}
-			jdbcTemplate.batchUpdate(UPDATESTMT, inputList);
-		} catch (Exception e) {
-			e.printStackTrace();
 		}
+		update = jdbcTemplate.batchUpdate(UPDATESTMT, inputList);
 
-		return "success";
+		if (update.length > 0) {
+			return true;
+		} else {
+			return false;
+		}
 
 	}
 
@@ -143,50 +111,41 @@ public class EmployeeStatusDaoImpl implements EmployeeStatusDao {
 	 * com.ojas.obs.dao.EmployeeStatusDao#deleteEmployeeContactInfo(com.ojas.obs.
 	 * requests.EmployeeContactInfoRequest)
 	 */
+
+	@Transactional(rollbackFor = Exception.class)
 	@Override
-	public String deleteEmployeeContactInfo(EmployeeContactInfoRequest empRequest) {
+	public boolean deleteEmployeeContactInfo(EmployeeContactInfoRequest employeeContactInfoRequest) {
 
-		logger.debug("@Deleting employee contact info");
+		logger.debug("@Deleting employee contact info" + employeeContactInfoRequest);
 		Timestamp sqlDeleteDate = new Timestamp(new java.util.Date().getTime());
-
+		int[] delete;
 		List<Object[]> inputList = new ArrayList<Object[]>();
-		for (EmployeeContactInfo emp : empRequest.getEmpInfo()) {
-			Object[] tmp = {
-
-					false, sqlDeleteDate, emp.getUpdated_By(), emp.getId()
-
-			};
+		for (EmployeeContactInfo emp : employeeContactInfoRequest.getEmpInfo()) {
+			Object[] tmp = { false, sqlDeleteDate, emp.getUpdatedBy(), emp.getId() };
 			inputList.add(tmp);
-
 		}
-		try {
-			int[] res = jdbcTemplate.batchUpdate(DELETECONTACT, inputList);
-
-			Boolean b = true;
-			for (int i = 0; i < res.length; i++) {
-				if (res[i] == 1) {
-
-				} else {
-					b = false;
-					break;
-				}
-
-				if (b) {
-					return "success";
-				}
-			}
-		} finally {
-			if (jdbcTemplate != null) {
-				try {
-					jdbcTemplate.getDataSource().getConnection().close();
-				} catch (Exception e) {
-
-					e.printStackTrace();
-				}
-
-			}
-			return "failed";
+		delete = jdbcTemplate.batchUpdate(DELETECONTACT, inputList);
+		if (delete.length > 0) {
+			return true;
+		} else {
+			return false;
 		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.ojas.obs.dao.EmployeeStatusDao#showEmployeeContactInfo(com.ojas.obs.
+	 * requests.EmployeeContactInfoRequest)
+	 */
+
+	@Override
+	public List<EmployeeContactInfo> showEmployeeContactInfoById(EmployeeContactInfoRequest empRequest) {
+		List<EmployeeContactInfo> list = null;
+		logger.debug("@getting employee contact info based on his id" + empRequest);
+		list = jdbcTemplate.query(SHOWCONTACTINFOSTMT, new Object[] { empRequest.getEmpInfo().get(0).getId() },
+				new EmployeeContactRowMapper());
+		return list;
 
 	}
 
@@ -200,63 +159,20 @@ public class EmployeeStatusDaoImpl implements EmployeeStatusDao {
 	public List<EmployeeContactInfo> getAll(EmployeeContactInfoRequest employeeContactInfo) {
 
 		logger.debug("@Getting all employees contact info details");
-		try {
-			return jdbcTemplate.query(TOTALRECORDS, new Object[] { true },
-					new BeanPropertyRowMapper<EmployeeContactInfo>(EmployeeContactInfo.class));
-		} finally {
-			if (jdbcTemplate != null) {
-				try {
-					jdbcTemplate.getDataSource().getConnection().close();
-				} catch (Exception e) {
+		List<EmployeeContactInfo> list = null;
+		list = jdbcTemplate.query(TOTALRECORDS, new EmployeeContactRowMapper());
 
-					e.printStackTrace();
-				}
-
-			}
-		}
+		return list;
 	}
 
 	@Override
-	public EmployeeContactInfo showEmployeeContactInfoByEmpId(EmployeeContactInfoRequest empRequest) {
-		EmployeeContactInfo emp = null;
+	public List<EmployeeContactInfo> showEmployeeContactInfoByEmpId(EmployeeContactInfoRequest empRequest) {
+		List<EmployeeContactInfo> list = null;
 		logger.debug("@getting employee contact info based on his empid");
-		try {
-			emp = jdbcTemplate.queryForObject(SHOWCONTACTINFOSTMTEMPID,
-					new Object[] { empRequest.getEmpInfo().get(0).getEmp_Id() }, new EmployeeContactRowMapper());
-		} finally {
-			if (jdbcTemplate != null) {
-				try {
-					jdbcTemplate.getDataSource().getConnection().close();
-				} catch (Exception e) {
+		list = jdbcTemplate.query(SHOWCONTACTINFOSTMTEMPID, new Object[] { empRequest.getEmpInfo().get(0).getEmpId() },
+				new EmployeeContactRowMapper());
+		return list;
 
-					e.printStackTrace();
-				}
-
-			}
-		}
-		return emp;
-
-	}
-
-	@Override
-	public EmployeeContactInfo showEmployeeContactInfoById(EmployeeContactInfoRequest empRequest) {
-		EmployeeContactInfo emp = null;
-		logger.debug("@getting employee contact info based on his empid");
-		try {
-			emp = jdbcTemplate.queryForObject(SHOWCONTACTINFOSTMTID,
-					new Object[] { empRequest.getEmpInfo().get(0).getId() }, new EmployeeContactRowMapper());
-		} finally {
-			if (jdbcTemplate != null) {
-				try {
-					jdbcTemplate.getDataSource().getConnection().close();
-				} catch (Exception e) {
-
-					e.printStackTrace();
-				}
-
-			}
-		}
-		return emp;
 	}
 
 }

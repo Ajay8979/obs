6097291d4c8647eda498ceaa18be6a752227c0bf
tshiva <tshiva@host.docker.internal>
2019-06-19 14:06:24 +0530

    removed getAllcount() method

--- zuul-auth-ojas-parent/ojas-obs-kye/src/main/java/com/ojas/obs/facade/KyeFacadeImpl.java
+++ zuul-auth-ojas-parent/ojas-obs-kye/src/main/java/com/ojas/obs/facade/KyeFacadeImpl.java
@@ -38,54 +38,48 @@ public class KyeFacadeImpl implements KyeFacade {
 		try {
 			if (kyeRequest.getTransactionType().equalsIgnoreCase(SAVE)) {
 				boolean saveKYE = kyeDao.saveKYE(kyeRequest);
-				int count = kyeDao.getAllKYECount();
 				if (saveKYE) {
 					KyeResponse response = new KyeResponse();
-					response.setStatusMessage("record added successfully");
+					response.setMessage("record added successfully");
 					response.setStatusCode("200");
-					response.setTotalCount(count);
 					return new ResponseEntity<>(response, HttpStatus.OK);
 				}
 				logger.debug("request is not valid");
 				ErrorResponse error = new ErrorResponse();
-				error.setErrorMessage("record not added");
-				error.setErrorCode("401");
+				error.setMessage("record not added");
+				error.setStatusCode("409");
 				return new ResponseEntity<>(error, HttpStatus.UNPROCESSABLE_ENTITY);
 			}
 			if (kyeRequest.getTransactionType().equalsIgnoreCase(UPDATE)) {
 				boolean updateKYE = kyeDao.updateKYE(kyeRequest);
-				int count = kyeDao.getAllKYECount();
 				if (updateKYE) {
 					KyeResponse response = new KyeResponse();
-					response.setStatusMessage("record updated successfully");
+					response.setMessage("record updated successfully");
 					response.setStatusCode("200");
-					response.setTotalCount(count);
 					return new ResponseEntity<>(response, HttpStatus.OK);
 				}
 				ErrorResponse error = new ErrorResponse();
-				error.setErrorMessage("record not updated");
-				error.setErrorCode("401");
+				error.setMessage("record not updated");
+				error.setStatusCode("401");
 				return new ResponseEntity<>(error, HttpStatus.UNPROCESSABLE_ENTITY);
 			}
 			if (kyeRequest.getTransactionType().equalsIgnoreCase(DELETE)) {
 				boolean deleteKYE = kyeDao.deleteKYE(kyeRequest);
-				int count = kyeDao.getAllKYECount();
 				if (deleteKYE) {
 					KyeResponse response = new KyeResponse();
-					response.setTotalCount(count);
-					response.setStatusMessage("record deleted successfully");
+					response.setMessage("record deleted successfully");
 					response.setStatusCode("200");
 					return new ResponseEntity<>(response, HttpStatus.OK);
 				}
 				ErrorResponse error = new ErrorResponse();
-				error.setErrorMessage("record not deleted");
-				error.setErrorCode("422");
+				error.setMessage("record not deleted");
+				error.setStatusCode("422");
 				return new ResponseEntity<>(error, HttpStatus.UNPROCESSABLE_ENTITY);
 			}
 		} catch (Exception e) {
 			ErrorResponse error = new ErrorResponse();
-			error.setErrorMessage(e.getMessage());
-			error.setErrorCode("409");
+			error.setMessage(e.getMessage());
+			error.setStatusCode("409");
 			return new ResponseEntity<>(error, HttpStatus.CONFLICT);
 		}
 		return null;
@@ -98,35 +92,23 @@ public class KyeFacadeImpl implements KyeFacade {
 		try {
 			if (kyeRequest.getTransactionType().equalsIgnoreCase(GETALL)) {
 				List<KYE> allKYE = kyeDao.getAllKYE(kyeRequest);
-				List<KYE> kYElist = kyeDao.getCountPerPage(allKYE, kyeRequest.getPageSize(), kyeRequest.getPageNo());
 
-				if ((allKYE == null || allKYE.isEmpty()) && (kYElist == null || kYElist.isEmpty())) {
+				if (allKYE == null || allKYE.isEmpty()) {
 					kyeResponse.setKyeList(new ArrayList<>());
-					kyeResponse.setStatusMessage("No records found");
-					kyeResponse.setTotalCount(0);
+					kyeResponse.setMessage("No records found");
+					kyeResponse.setStatusCode("422");
 					return new ResponseEntity<>(kyeResponse, HttpStatus.UNPROCESSABLE_ENTITY);
 				} else {
-					int count = kyeDao.getAllKYECount();
-					if (kyeRequest.getPageNo() == 0 || kyeRequest.getPageSize() == 0) {
-						kyeResponse.setKyeList(allKYE);
-						kyeResponse.setStatusMessage("success");
-						kyeResponse.setTotalCount(count);
-						return new ResponseEntity<>(kyeResponse, HttpStatus.OK);
-					} else {
-						kyeResponse.setKyeList(kYElist);
-						kyeResponse.setStatusMessage("success");
-						kyeResponse.setStatusCode("200");
-						kyeResponse.setTotalCount(count);
-						kyeResponse.setPageNo(kyeRequest.getPageNo());
-						kyeResponse.setPageSize(kyeRequest.getPageSize());
-						return new ResponseEntity<>(kyeResponse, HttpStatus.OK);
-					}
+					kyeResponse.setKyeList(allKYE);
+					kyeResponse.setMessage("success");
+					kyeResponse.setStatusCode("200");
+					return new ResponseEntity<>(kyeResponse, HttpStatus.OK);
 				}
 			}
 		} catch (Exception e) {
 			ErrorResponse error = new ErrorResponse();
-			error.setErrorMessage(e.getMessage());
-			error.setErrorCode("409");
+			error.setMessage(e.getMessage());
+			error.setStatusCode("409");
 			return new ResponseEntity<>(error, HttpStatus.CONFLICT);
 		}
 		return null;

0e208e0b7261c3690407e57f0bae0535fcc4729e
tshiva <tshiva@host.docker.internal>
2019-06-19 12:46:57 +0530

    added logger  messages

--- zuul-auth-ojas-parent/ojas-obs-employeeskills/src/main/java/com/ojas/obs/facade/EmployeeSkillFacadeImpl.java
+++ zuul-auth-ojas-parent/ojas-obs-employeeskills/src/main/java/com/ojas/obs/facade/EmployeeSkillFacadeImpl.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,12 +21,15 @@ public class EmployeeSkillFacadeImpl implements EmployeeSkillFacade {
 	@Autowired
 	private EmployeeSkillDao employeeSkillDao;
 
+	Logger logger = Logger.getLogger(this.getClass());
+
 	@Override
 	public ResponseEntity<Object> setEmployeeSkillInfo(EmployeeSkillInfoRequest employeeSkillInfoRequest)
 			throws SQLException {
 
 		EmployeeSkillInfoResponse response = new EmployeeSkillInfoResponse();
 		List<EmployeeSkillInfo> listEmployeeSkillInfo = employeeSkillInfoRequest.getSkillInfoModel();
+		logger.info("Enter the set method facade ....");
 
 		// save method
 
@@ -34,6 +38,7 @@ public class EmployeeSkillFacadeImpl implements EmployeeSkillFacade {
 			employeeSkillDao.saveEmployeeSkillInfo(employeeSkillInfoRequest);
 			int count = employeeSkillDao.getAllCount();
 			response.setGetSkillInfoList(new ArrayList<>());
+			logger.debug("Successfully record addedd...");
 			response.setStatusMessage("Successfully record added");
 			response.setTotalCount(count);
 
@@ -48,6 +53,7 @@ public class EmployeeSkillFacadeImpl implements EmployeeSkillFacade {
 					employeeSkillDao.updateEmployeeSkillInfo(employeeSkillInfoRequest);
 					int count = employeeSkillDao.getAllCount();
 					response.setGetSkillInfoList(new ArrayList<>());
+					logger.debug("Successfully record updated....");
 					response.setStatusMessage("Successfully record updated");
 					response.setTotalCount(count);
 					return new ResponseEntity<Object>(response, HttpStatus.OK);
@@ -60,20 +66,23 @@ public class EmployeeSkillFacadeImpl implements EmployeeSkillFacade {
 	public ResponseEntity<Object> getEmployeeSkillInfo(EmployeeSkillInfoRequest employeeSkillInfoRequest)
 			throws SQLException {
 
+		logger.info("Enter the get method () in facade class....");
+		
 		// -------------getById method call----------------------------//
 
 		EmployeeSkillInfoResponse response = new EmployeeSkillInfoResponse();
 		List<EmployeeSkillInfo> listEmployeeSkillInfo = employeeSkillInfoRequest.getSkillInfoModel();
-		List<EmployeeSkillInfo> byId=null;
+		List<EmployeeSkillInfo> byId = null;
 		if (employeeSkillInfoRequest.getTransactionType().equalsIgnoreCase("getById")) {
 			for (EmployeeSkillInfo skillDetails : listEmployeeSkillInfo) {
-				if(skillDetails.getId()==null)
-					byId = employeeSkillDao.getByEmpId(skillDetails.getEmployee_id());	
+				if (skillDetails.getId() == null)
+					byId = employeeSkillDao.getByEmpId(skillDetails.getEmployee_id());
 				else
 					byId = employeeSkillDao.getById(skillDetails.getId());
 				int count = employeeSkillDao.getAllCount();
 				response.setTotalCount(count);
 				response.setGetSkillInfoList(byId);
+				logger.debug(" success");
 				response.setStatusMessage("success");
 				return new ResponseEntity<Object>(response, HttpStatus.OK);
 			}

3a3e2be3679889a5b6d18094e9dc09650b3951b6
tshiva <tshiva@host.docker.internal>
2019-06-19 12:50:38 +0530

    removed try and catch block

--- zuul-auth-ojas-parent/ojas-obs-employment-details/src/main/java/com/ojas/obs/employmentdetails/dao/EmploymentDetailsDAOImpl.java
+++ zuul-auth-ojas-parent/ojas-obs-employment-details/src/main/java/com/ojas/obs/employmentdetails/dao/EmploymentDetailsDAOImpl.java
@@ -48,32 +48,24 @@ public class EmploymentDetailsDAOImpl implements EmploymentDetailsDAO {
 	@Transactional(rollbackFor = DataNotInsertedException.class)
 	public boolean saveEmploymentDetails(List<EmploymentDetails> employmentDetailsList)
 			throws DataNotInsertedException {
+
 		Timestamp created_date = new Timestamp(new Date().getTime());
 		int savedRows = 0;
-		try {
-			for (EmploymentDetails employmentObject : employmentDetailsList) {
 
-				savedRows = jdbcTemplate.update(queryUtil.getQuery("INSERT_EMPLOYMENT_DETAILS_STMT"),
-						employmentObject.getEmployeeId(), employmentObject.getJoiningDate(),
-						employmentObject.getResourceType(), employmentObject.isBondStatus(),
-						employmentObject.getResignationDate(), employmentObject.getExitDate(),
-						employmentObject.getSeparationType(), 1, employmentObject.getCreatedBy(), created_date);
-			}
-			if (savedRows > 0) {
-				LOGGER.debug("Employee details record inserted succefully ");
-				return true;
-			}
-			LOGGER.debug("Employee details record not inserted succefully ");
-			return false;
-		} finally {
-			if (null != jdbcTemplate) {
-				try {
-					jdbcTemplate.getDataSource().getConnection().close();
-				} catch (Exception exception) {
-					exception.getMessage();
-				}
-			}
+		for (EmploymentDetails employmentObject : employmentDetailsList) {
+
+			savedRows = jdbcTemplate.update(queryUtil.getQuery("INSERT_EMPLOYMENT_DETAILS_STMT"),
+					employmentObject.getEmployeeId(), employmentObject.getJoiningDate(),
+					employmentObject.getResourceType(), employmentObject.isBondStatus(),
+					employmentObject.getResignationDate(), employmentObject.getExitDate(),
+					employmentObject.getSeparationType(), 1, employmentObject.getCreatedBy(), created_date);
 		}
+		if (savedRows > 0) {
+			LOGGER.debug("Employee details record inserted succefully ");
+			return true;
+		}
+		LOGGER.debug("Employee details record not inserted succefully ");
+		return false;
 	}
 
 	/**
@@ -87,46 +79,28 @@ public class EmploymentDetailsDAOImpl implements EmploymentDetailsDAO {
 		boolean flag = true;
 		int updatedRows = 0;
 		Timestamp updatedDate_date = new Timestamp(new Date().getTime());
-		try {
-			for (EmploymentDetails employmentObject : employmentDetailsList) {
-				updatedRows = jdbcTemplate.update(queryUtil.getQuery("UPDATE_EMPLOYMENT_DETAILS_STMT"),
-						employmentObject.getResourceType(), employmentObject.isBondStatus(),
-						employmentObject.getResignationDate(), employmentObject.getExitDate(),
-						employmentObject.getSeparationType(), flag, employmentObject.getUpdatedBy(), updatedDate_date,
-						employmentObject.getId());
-			}
-			if (updatedRows > 0) {
-				return true;
-			}
-			return false;
-		} finally {
-			if (null != jdbcTemplate) {
-				try {
-					jdbcTemplate.getDataSource().getConnection().close();
-				} catch (Exception exception) {
-					exception.getMessage();
-				}
-			}
+
+		for (EmploymentDetails employmentObject : employmentDetailsList) {
+			updatedRows = jdbcTemplate.update(queryUtil.getQuery("UPDATE_EMPLOYMENT_DETAILS_STMT"),
+					employmentObject.getResourceType(), employmentObject.isBondStatus(),
+					employmentObject.getResignationDate(), employmentObject.getExitDate(),
+					employmentObject.getSeparationType(), flag, employmentObject.getUpdatedBy(), updatedDate_date,
+					employmentObject.getId());
 		}
+		if (updatedRows > 0) {
+			return true;
+		}
+		return false;
 	}
 
 	private boolean isEmployeeExits(Integer id) {
-		try {
-			int count = jdbcTemplate.queryForObject("select count(*) from employment_details where id=?", Integer.class,
-					id);
-			if (count == 0) {
-				return false;
-			} else {
-				return true;
-			}
-		} finally {
-			if (null != jdbcTemplate) {
-				try {
-					jdbcTemplate.getDataSource().getConnection().close();
-				} catch (Exception exception) {
-					exception.getMessage();
-				}
-			}
+
+		int count = jdbcTemplate.queryForObject("select count(*) from employment_details where id=?", Integer.class,
+				id);
+		if (count == 0) {
+			return false;
+		} else {
+			return true;
 		}
 	}
 
@@ -140,24 +114,15 @@ public class EmploymentDetailsDAOImpl implements EmploymentDetailsDAO {
 			throws DataNotInsertedException {
 		int deletedRows = 0;
 		boolean flag = false;
-		try {
-			for (EmploymentDetails employmentObject : employmentDetailsList) {
-				deletedRows = jdbcTemplate.update(queryUtil.getQuery("DELETE_EMPLOYMENT_DETAILS_STMT"), flag,
-						employmentObject.getId());
-			}
-			if (deletedRows > 0) {
-				return true;
-			}
-			return false;
-		} finally {
-			if (null != jdbcTemplate) {
-				try {
-					jdbcTemplate.getDataSource().getConnection().close();
-				} catch (Exception exception) {
-					exception.getMessage();
-				}
-			}
+
+		for (EmploymentDetails employmentObject : employmentDetailsList) {
+			deletedRows = jdbcTemplate.update(queryUtil.getQuery("DELETE_EMPLOYMENT_DETAILS_STMT"), flag,
+					employmentObject.getId());
 		}
+		if (deletedRows > 0) {
+			return true;
+		}
+		return false;
 	}
 
 	/**
@@ -167,22 +132,11 @@ public class EmploymentDetailsDAOImpl implements EmploymentDetailsDAO {
 	public List<EmploymentDetails> getAllEmploymentDetails() {
 
 		RowMapper<EmploymentDetails> rowMapper = new EmploymentDetailsRowMapper();
-		try {
-			List<EmploymentDetails> employmentDetailsList = jdbcTemplate
-					.query(queryUtil.getQuery("GET_EMPLOYMENT_DETAILS_STMT"), rowMapper);
-			LOGGER.debug("Employee  details list " + employmentDetailsList);
-			return employmentDetailsList;
-		} finally {
-			if (null != jdbcTemplate) {
-				try {
-					jdbcTemplate.getDataSource().getConnection().close();
-				} catch (Exception exception) {
-					exception.getMessage();
-				}
-			}
-		}
+		List<EmploymentDetails> employmentDetailsList = jdbcTemplate
+				.query(queryUtil.getQuery("GET_EMPLOYMENT_DETAILS_STMT"), rowMapper);
+		LOGGER.debug("Employee  details list " + employmentDetailsList);
+		return employmentDetailsList;
 	}
-
 
 	@Override
 	public List<EmploymentDetails> getEmploymentDetailsByEmploymentId(
@@ -208,57 +162,39 @@ public class EmploymentDetailsDAOImpl implements EmploymentDetailsDAO {
 //
 //			LOGGER.debug("Employee  details list " + employmentDetailsList);
 //			return employmentDetailsList;
-		
-		
+
 		List<EmploymentDetails> bankDetails = employmentDetailsRequest.getEmploymentDetails();
 
 		List<EmploymentDetails> list = new ArrayList<EmploymentDetails>();
-		try {
-			List<EmploymentDetails> query = jdbcTemplate.query(
-				queryUtil.getQuery("GET_EMPLOYMENT_DETAILS_BY_EMPLOYEE_ID_STMT"),
-					new BeanPropertyRowMapper<EmploymentDetails>(EmploymentDetails.class));
+		String query2 = queryUtil.getQuery("GET_EMPLOYMENT_DETAILS_BY_EMPLOYEE_ID_STMT");
+		query2 = query2+ "and employee_id = "+ employmentDetailsRequest.getEmploymentDetails().get(0).getEmployeeId();
+		List<EmploymentDetails> query = jdbcTemplate.query(query2,new BeanPropertyRowMapper<EmploymentDetails>(EmploymentDetails.class));
+		if(!query.isEmpty()) {
 			for (EmploymentDetails bankDetails2 : query) {
 				for (EmploymentDetails bankDetails3 : bankDetails) {
 					if (bankDetails2.getId() == bankDetails3.getId()) {
 						list.add(bankDetails2);
 						return list;
-					}else if (bankDetails2.getEmployeeId().equals(bankDetails3.getEmployeeId())) {
+					} else if (bankDetails2.getEmployeeId().equals(bankDetails3.getEmployeeId())) {
 						list.add(bankDetails2);
 						return list;
 					}
 				}
 			}
-
-			return query;
-		} finally {
-			if (null != jdbcTemplate) {
-				try {
-					jdbcTemplate.getDataSource().getConnection().close();
-				} catch (Exception exception) {
-					exception.getMessage();
-				}
-			}
 		}
+		
+		
+
+		return query;
 	}
 
 	@Override
 	public List<EmploymentDetails> getEmploymentDetailsById(Integer id) {
 
 		RowMapper<EmploymentDetails> rowMapper = new EmploymentDetailsRowMapper();
-		try{
 		List<EmploymentDetails> employmentDetailsList = jdbcTemplate
 				.query(queryUtil.getQuery("GET_EMPLOYMENT_DETAILS_BY_ID_STMT"), new Object[] { id }, rowMapper);
 		LOGGER.debug("Employee  details list " + employmentDetailsList);
 		return employmentDetailsList;
-		} finally {
-			if (null != jdbcTemplate) {
-				try {
-					jdbcTemplate.getDataSource().getConnection().close();
-				} catch (Exception exception) {
-					exception.getMessage();
-				}
-			}
-		}
 	}
-
 }

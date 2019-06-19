69e141a363729068248e7811cd443d3935879b4e
tshiva <tshiva@host.docker.internal>
2019-06-19 12:20:03 +0530

    removed pageNO and pageSize fields

--- zuul-auth-ojas-parent/ojas-obs-employee-education-details/src/main/java/com/ojas/obs/emp_edu/model/EmployeeEducationDetailsRequest.java
+++ zuul-auth-ojas-parent/ojas-obs-employee-education-details/src/main/java/com/ojas/obs/emp_edu/model/EmployeeEducationDetailsRequest.java
@@ -5,32 +5,18 @@ import java.util.List;
 public class EmployeeEducationDetailsRequest {
 
 	private List<EmployeeEducationDetails> employeeEducationDetailsList;
-	private String transaactionType;
-	private Integer pageNo;
-	private Integer pageSize;
+	private String transactionType;
 	public List<EmployeeEducationDetails> getEmployeeEducationDetailsList() {
 		return employeeEducationDetailsList;
 	}
 	public void setEmployeeEducationDetailsList(List<EmployeeEducationDetails> employeeEducationDetailsList) {
 		this.employeeEducationDetailsList = employeeEducationDetailsList;
 	}
-	public String getTransaactionType() {
-		return transaactionType;
+	public String getTransactionType() {
+		return transactionType;
 	}
-	public void setTransaactionType(String transaactionType) {
-		this.transaactionType = transaactionType;
-	}
-	public Integer getPageNo() {
-		return pageNo;
-	}
-	public void setPageNo(Integer pageNo) {
-		this.pageNo = pageNo;
-	}
-	public Integer getPageSize() {
-		return pageSize;
-	}
-	public void setPageSize(Integer pageSize) {
-		this.pageSize = pageSize;
+	public void setTransactionType(String transactionType) {
+		this.transactionType = transactionType;
 	}
 	@Override
 	public int hashCode() {
@@ -38,9 +24,7 @@ public class EmployeeEducationDetailsRequest {
 		int result = 1;
 		result = prime * result
 				+ ((employeeEducationDetailsList == null) ? 0 : employeeEducationDetailsList.hashCode());
-		result = prime * result + ((pageNo == null) ? 0 : pageNo.hashCode());
-		result = prime * result + ((pageSize == null) ? 0 : pageSize.hashCode());
-		result = prime * result + ((transaactionType == null) ? 0 : transaactionType.hashCode());
+		result = prime * result + ((transactionType == null) ? 0 : transactionType.hashCode());
 		return result;
 	}
 	@Override
@@ -57,23 +41,12 @@ public class EmployeeEducationDetailsRequest {
 				return false;
 		} else if (!employeeEducationDetailsList.equals(other.employeeEducationDetailsList))
 			return false;
-		if (pageNo == null) {
-			if (other.pageNo != null)
+		if (transactionType == null) {
+			if (other.transactionType != null)
 				return false;
-		} else if (!pageNo.equals(other.pageNo))
-			return false;
-		if (pageSize == null) {
-			if (other.pageSize != null)
-				return false;
-		} else if (!pageSize.equals(other.pageSize))
-			return false;
-		if (transaactionType == null) {
-			if (other.transaactionType != null)
-				return false;
-		} else if (!transaactionType.equals(other.transaactionType))
+		} else if (!transactionType.equals(other.transactionType))
 			return false;
 		return true;
 	}
-	
 	
 }

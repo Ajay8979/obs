0b377bfbb8dfbe5d22ceef6c35dfe6672a1c1863
tshiva <tshiva@host.docker.internal>
2019-06-19 16:07:49 +0530

    added sql exception

--- zuul-auth-ojas-parent/ojas-obs-resource-type/src/main/java/com/ojas/obs/resourcetype/dao/ResourceTypeDAO.java
+++ zuul-auth-ojas-parent/ojas-obs-resource-type/src/main/java/com/ojas/obs/resourcetype/dao/ResourceTypeDAO.java
@@ -1,22 +1,19 @@
 package com.ojas.obs.resourcetype.dao;
 
+import java.sql.SQLException;
 import java.util.List;
 
-import com.ojas.obs.resourcetype.exception.DataNotInsertedException;
 import com.ojas.obs.resourcetype.model.ResourceType;
 
 public interface ResourceTypeDAO {
 	
-	  boolean saveResourceTypes(List<ResourceType> resourceTypes) throws
-	  DataNotInsertedException;
+	  boolean saveResourceTypes(List<ResourceType> resourceTypes) throws SQLException;
 	  
-	  boolean updateResourceTypes(List<ResourceType> resourceTypes) throws
-	  DataNotInsertedException;
+	  boolean updateResourceTypes(List<ResourceType> resourceTypes) throws SQLException;
 	  
-	  boolean deleteResourceTypes(List<ResourceType> resourceTypes) throws
-	  DataNotInsertedException;
+	  boolean deleteResourceTypes(List<ResourceType> resourceTypes) throws SQLException;
 	 
-	List<ResourceType> getAllResourceTypes();
+	List<ResourceType> getAllResourceTypes() throws SQLException;
 
-	List<ResourceType> getResourceTypeById(Integer employeeId);
+	List<ResourceType> getResourceTypeById(Integer employeeId) throws SQLException;
 }

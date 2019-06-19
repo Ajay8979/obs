f8e6b902693f9779cd4562c60f39bfa05b578723
tshiva <tshiva@host.docker.internal>
2019-06-19 17:13:54 +0530

    removed sessionId

--- zuul-auth-ojas-parent/ojas-obs-online-employeestatus-service/src/main/java/com/ojas/obs/request/EmployeeStatusRequest.java
+++ zuul-auth-ojas-parent/ojas-obs-online-employeestatus-service/src/main/java/com/ojas/obs/request/EmployeeStatusRequest.java
@@ -10,18 +10,10 @@ import com.ojas.obs.model.EmployeeStatus;
  *
  */
 public class EmployeeStatusRequest {
-	private String sessionId;
+
 	private List<EmployeeStatus> employeeStatus;
-	private String statusCode;
+
 	private String transactionType;
-
-	public String getSessionId() {
-		return sessionId;
-	}
-
-	public void setSessionId(String sessionId) {
-		this.sessionId = sessionId;
-	}
 
 	public List<EmployeeStatus> getEmployeeStatus() {
 		return employeeStatus;
@@ -29,14 +21,6 @@ public class EmployeeStatusRequest {
 
 	public void setEmployeeStatus(List<EmployeeStatus> employeeStatus) {
 		this.employeeStatus = employeeStatus;
-	}
-
-	public String getStatusCode() {
-		return statusCode;
-	}
-
-	public void setStatusCode(String statusCode) {
-		this.statusCode = statusCode;
 	}
 
 	public String getTransactionType() {
@@ -48,12 +32,15 @@ public class EmployeeStatusRequest {
 	}
 
 	@Override
+	public String toString() {
+		return "EmployeeStatusRequest [employeeStatus=" + employeeStatus + ", transactionType=" + transactionType + "]";
+	}
+
+	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((statusCode == null) ? 0 : statusCode.hashCode());
 		result = prime * result + ((employeeStatus == null) ? 0 : employeeStatus.hashCode());
-		result = prime * result + ((sessionId == null) ? 0 : sessionId.hashCode());
 		result = prime * result + ((transactionType == null) ? 0 : transactionType.hashCode());
 		return result;
 	}
@@ -67,31 +54,19 @@ public class EmployeeStatusRequest {
 		if (getClass() != obj.getClass())
 			return false;
 		EmployeeStatusRequest other = (EmployeeStatusRequest) obj;
-		if (statusCode == null) {
-			if (other.statusCode != null)
-				return false;
-		} else if (!statusCode.equals(other.statusCode)) {
-			return false;
-		}
 		if (employeeStatus == null) {
 			if (other.employeeStatus != null)
 				return false;
-		} else if (!employeeStatus.equals(other.employeeStatus)) {
+		} else if (!employeeStatus.equals(other.employeeStatus))
 			return false;
-		}
-		if (sessionId == null) {
-			if (other.sessionId != null)
-				return false;
-		} else if (!sessionId.equals(other.sessionId)) {
-			return false;
-		}
 		if (transactionType == null) {
 			if (other.transactionType != null)
 				return false;
-		} else if (!transactionType.equals(other.transactionType)) {
+		} else if (!transactionType.equals(other.transactionType))
 			return false;
-		}
 		return true;
 	}
+	
+	
 
 }

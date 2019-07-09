1db33b61b4e2fea1bb3593a742d808d9b82ab09f
Radhika Challa <radhika.challa@ojas-it.com>
2019-07-09 09:29:00 +0000

    career.component.ts added project details

--- zuul-auth-ojas-parent/ui-parent/ui-web/src/main/web/src/app/home/career/career.component.ts
+++ zuul-auth-ojas-parent/ui-parent/ui-web/src/main/web/src/app/home/career/career.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HrmsService } from '../services/hrms.service';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-career',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./career.component.scss']
 })
 export class CareerComponent implements OnInit {
-
-  constructor() { }
+  projectDetails: any;
+  projectDetailsLi: any;
+  private pageSize: number = 5;
+  constructor(private hrms:HrmsService, private dataservice:DataService) { }
 
   ngOnInit() {
+    this.getProject();
   }
-
+  getProject(){
+    var jsonData =
+    {
+      "projectDetailsList" : [], 
+          "transactionType":"getAll"
+     }
+  //    {
+     
+  //     "transactionType":"getAll"
+  // }
+    this.hrms.getProjectDetails(jsonData).subscribe(response =>{
+      this.projectDetails = response;
+      this.projectDetailsLi = this. projectDetails.projectDetailsList;
+      console.log("Project Details List", this.projectDetailsLi);
+    })
+    
+    }
 }
